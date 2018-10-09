b66d677602f2e6e0fa818adc0569b74ade16bff3
btopro <btopro@outlook.com>
2018-10-09 09:33:11 -0400

    automated modulizer starting point for beaker-broker

---
+++ elements/beaker-broker/build/es6/beaker-broker.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { BeakerBroker };
+class BeakerBroker extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Beaker broker",
+        description: "Automated conversion of beaker-broker",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Broker"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "beaker-broker";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      BeakerBroker.haxProperties,
+      BeakerBroker.tag,
+      this
+    );
+  }
+}
+window.customElements.define(BeakerBroker.tag, BeakerBroker);
