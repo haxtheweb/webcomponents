3b79f341e6c993ff1436047a15c58856287389de
btopro <btopro@outlook.com>
2018-10-23 14:20:30 -0400

    automated modulizer starting point for hax-app/

---
+++ elements/hax-app/build/es6/hax-app.js
@@ -0,0 +1,49 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { HaxApp };
+class HaxApp extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Hax app",
+        description: "Automated conversion of hax-app/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["App"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "hax-app";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(HaxApp.haxProperties, HaxApp.tag, this);
+  }
+}
+window.customElements.define(HaxApp.tag, HaxApp);
