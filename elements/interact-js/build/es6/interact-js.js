5c4a7fc45e854ea79beda20b3ee22bb6fe7e6c65
btopro <btopro@outlook.com>
2018-10-23 14:36:39 -0400

    automated modulizer starting point for interact-js/

---
+++ elements/interact-js/build/es6/interact-js.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { InteractJs };
+class InteractJs extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Interact js",
+        description: "Automated conversion of interact-js/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Js"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "interact-js";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      InteractJs.haxProperties,
+      InteractJs.tag,
+      this
+    );
+  }
+}
+window.customElements.define(InteractJs.tag, InteractJs);
---
+++ elements/interact-js/lib/interact-js.js
@@ -0,0 +1,1 @@
+import "./src/interact.min.js";
