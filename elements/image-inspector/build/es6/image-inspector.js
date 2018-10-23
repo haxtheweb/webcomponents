1222baa9b22d4c6e3abb9f482aaa14a8c2f9645a
btopro <btopro@outlook.com>
2018-10-23 14:35:14 -0400

    automated modulizer starting point for image-inspector/

---
+++ elements/image-inspector/build/es6/image-inspector.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { ImageInspector };
+class ImageInspector extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Image inspector",
+        description: "Automated conversion of image-inspector/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Inspector"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "image-inspector";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      ImageInspector.haxProperties,
+      ImageInspector.tag,
+      this
+    );
+  }
+}
+window.customElements.define(ImageInspector.tag, ImageInspector);
