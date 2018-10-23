5765c869ef7bf845c6380aaac54160eb01ee5758
btopro <btopro@outlook.com>
2018-10-23 14:56:52 -0400

    automated modulizer starting point for lrnapp-book/

---
+++ elements/lrnapp-book/lib/src/lrnapp-book/imports.js
@@ -0,0 +1,26 @@
+import "@polymer/polymer/polymer.js";
+import "@polymer/iron-icons/iron-icons.js";
+import "@polymer/iron-icons/hardware-icons.js";
+import "@polymer/iron-ajax/iron-ajax.js";
+import "@polymer/paper-icon-button/paper-icon-button.js";
+import "@polymer/paper-styles/color.js";
+import "paper-search/paper-search-bar.js";
+import "@polymer/paper-tooltip/paper-tooltip.js";
+import "@polymer/paper-slider/paper-slider.js";
+import "@polymer/app-layout/app-layout.js";
+import "@polymer/app-layout/app-drawer/app-drawer.js";
+import "@polymer/app-layout/app-header/app-header.js";
+import "@polymer/app-layout/app-toolbar/app-toolbar.js";
+import "@polymer/app-layout/app-scroll-effects/app-scroll-effects.js";
+import "@polymer/app-route/app-location.js";
+import "@polymer/app-route/app-route.js";
+import "materializecss-styles/materializecss-styles.js";
+import "lrndesign-stepper/lrndesign-stepper.js";
+import "lrnsys-progress/lrnsys-progress.js";
+import "elmsln-loading/elmsln-loading.js";
+import "page-scroll-position/page-scroll-position.js";
+import "hax-body/hax-body.js";
+import "material-progress/material-progress.js";
+import "lrndesign-mapmenu/lrndesign-mapmenu.js";
+
+/* HAX */
---
+++ elements/lrnapp-book/lrnapp-book.es6.js
@@ -0,0 +1,10 @@
+import{html,PolymerElement}from"./node_modules/@polymer/polymer/polymer-element.js";import{HAXWiring}from"./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";export{LrnappBook};class LrnappBook extends PolymerElement{static get template(){return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get haxProperties(){return{canScale:!0,canPosition:!0,canEditSource:!1,gizmo:{title:"Lrnapp book",description:"Automated conversion of lrnapp-book/",icon:"icons:android",color:"green",groups:["Book"],handles:[{type:"todo:read-the-docs-for-usage"}],meta:{author:"btopro",owner:"The Pennsylvania State University"}},settings:{quick:[],configure:[],advanced:[]}}}static get properties(){return{}}static get tag(){return"lrnapp-book"}connectedCallback(){super.connectedCallback();this.HAXWiring=new HAXWiring;this.HAXWiring.setHaxProperties(LrnappBook.haxProperties,LrnappBook.tag,this)}}window.customElements.define(LrnappBook.tag,LrnappBook);
