9f979e8407c29a2272c16d98395d5ce123eee9db
btopro <btopro@outlook.com>
2018-10-23 14:48:57 -0400

    automated modulizer starting point for lrn-icon/

---
+++ elements/lrn-icon/src/lrn-icon.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrnIcon };
+/**
+ * `lrn-icon`
+ * `Automated conversion of lrn-icon/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class LrnIcon extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "lrn-icon";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(LrnIcon.haxProperties, LrnIcon.tag, this);
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(LrnIcon.tag, LrnIcon);
