dc0bf9affe3555f46aea46c3fee2eba1a8b4cb9e
btopro <btopro@outlook.com>
2018-10-23 15:21:19 -0400

    automated modulizer starting point for lrndesign-drawer/

---
+++ elements/lrndesign-drawer/lib/lrndesign-drawer.js
@@ -0,0 +1,83 @@
+import "@polymer/polymer/polymer.js";
+import "@polymer/app-layout/app-layout.js";
+import "@polymer/paper-icon-button/paper-icon-button.js";
+import "@polymer/paper-tooltip/paper-tooltip.js";
+/**
+`lrndesign-drawer`
+
+
+@demo demo/index.html
+*/
+Polymer({
+  _template: `
+    <style>
+      :host {
+        display: block;
+        --lrndesign-drawer-width: 30%;
+      }
+      app-header {
+        z-index: 100;
+      }
+      app-drawer {
+        --app-drawer-width: var(--lrndesign-drawer-width);
+        --app-drawer-content-container: {
+          padding: 1em;
+          overflow-y: scroll;
+          margin-top: 7em;
+        }
+      }
+    </style>
+    <app-header>
+      <app-drawer opened="{{opened}}" align="{{align}}">
+        <slot></slot>
+      </app-drawer>
+    </app-header>
+    <paper-icon-button icon="[[icon]]" alt="[[alt]]" id="flyout-drawer"></paper-icon-button>
+    <paper-tooltip for="flyout-drawer">[[alt]]</paper-tooltip>
+`,
+
+  is: "lrndesign-drawer",
+
+  properties: {
+    /**
+     * State for if it is currently open.
+     */
+    opened: {
+      type: Boolean,
+      value: false
+    },
+    /**
+     * Icon to present for clicking.
+     */
+    icon: {
+      type: String,
+      value: "icon"
+    },
+    /**
+     * Side of the screen to align the flyout (right or left)
+     */
+    align: {
+      type: String,
+      value: "left"
+    },
+    /**
+     * Alt / hover text for this link
+     */
+    alt: {
+      type: String,
+      value: ""
+    }
+  },
+
+  /**
+   * Initalize the flyout and ensure it's not open to start
+   * while adding the click event to it.
+   */
+  ready: function() {
+    let root = this;
+    let opened = this.opened;
+    this.$$("paper-icon-button").addEventListener("click", function(e) {
+      root.opened = !root.opened;
+    });
+  }
+});
---
+++ elements/lrndesign-drawer/lrndesign-drawer.es6.js
@@ -0,0 +1,10 @@
+import{html,PolymerElement}from"./node_modules/@polymer/polymer/polymer-element.js";import{HAXWiring}from"./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";export{LrndesignDrawer};class LrndesignDrawer extends PolymerElement{static get template(){return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get haxProperties(){return{canScale:!0,canPosition:!0,canEditSource:!1,gizmo:{title:"Lrndesign drawer",description:"Automated conversion of lrndesign-drawer/",icon:"icons:android",color:"green",groups:["Drawer"],handles:[{type:"todo:read-the-docs-for-usage"}],meta:{author:"btopro",owner:"The Pennsylvania State University"}},settings:{quick:[],configure:[],advanced:[]}}}static get properties(){return{}}static get tag(){return"lrndesign-drawer"}connectedCallback(){super.connectedCallback();this.HAXWiring=new HAXWiring;this.HAXWiring.setHaxProperties(LrndesignDrawer.haxProperties,LrndesignDrawer.tag,this)}}window.customElements.define(LrndesignDrawer.tag,LrndesignDrawer);
