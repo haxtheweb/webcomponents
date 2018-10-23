9ae0129dbaf1261a63d824f7e6e6a60b051da236
btopro <btopro@outlook.com>
2018-10-23 14:38:08 -0400

    automated modulizer starting point for item-overlay-ops/

---
+++ elements/item-overlay-ops/item-overlay-ops.es6.js
@@ -0,0 +1,10 @@
+import{html,PolymerElement}from"./node_modules/@polymer/polymer/polymer-element.js";import{HAXWiring}from"./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";export{ItemOverlayOps};class ItemOverlayOps extends PolymerElement{static get template(){return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get haxProperties(){return{canScale:!0,canPosition:!0,canEditSource:!1,gizmo:{title:"Item overlay-ops",description:"Automated conversion of item-overlay-ops/",icon:"icons:android",color:"green",groups:["Overlay"],handles:[{type:"todo:read-the-docs-for-usage"}],meta:{author:"btopro",owner:"The Pennsylvania State University"}},settings:{quick:[],configure:[],advanced:[]}}}static get properties(){return{}}static get tag(){return"item-overlay-ops"}connectedCallback(){super.connectedCallback();this.HAXWiring=new HAXWiring;this.HAXWiring.setHaxProperties(ItemOverlayOps.haxProperties,ItemOverlayOps.tag,this)}}window.customElements.define(ItemOverlayOps.tag,ItemOverlayOps);
