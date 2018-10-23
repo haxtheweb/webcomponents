ffe43f3a63702973193ce153bdccb90b230630e1
btopro <btopro@outlook.com>
2018-10-23 15:12:25 -0400

    automated modulizer starting point for lrndesign-chart/

---
+++ elements/lrndesign-chart/src/lrndesign-chart.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrndesignChart };
+/**
+ * `lrndesign-chart`
+ * `Automated conversion of lrndesign-chart/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class LrndesignChart extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "lrndesign-chart";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      LrndesignChart.haxProperties,
+      LrndesignChart.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(LrndesignChart.tag, LrndesignChart);
