8853f6b3f3030327b8875db8d0a4d7152eedc11d
btopro <btopro@outlook.com>
2018-10-23 17:49:02 -0400

    automated modulizer starting point for wysiwyg-hax/

---
+++ elements/wysiwyg-hax/build/es6/wysiwyg-hax.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { WysiwygHax };
+class WysiwygHax extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Wysiwyg hax",
+        description: "Automated conversion of wysiwyg-hax/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Hax"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "wysiwyg-hax";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      WysiwygHax.haxProperties,
+      WysiwygHax.tag,
+      this
+    );
+  }
+}
+window.customElements.define(WysiwygHax.tag, WysiwygHax);
---
+++ elements/wysiwyg-hax/lib/wysiwyg-hax.js
@@ -0,0 +1,181 @@
+import "@polymer/polymer/polymer.js";
+import "cms-hax/cms-hax.js";
+/*
+`wysiwyg-hax`
+Integration of wysiwyg edit form for a page with HAX.
+
+*/
+Polymer({
+  _template: `
+    <style>
+      :host {
+        display: block;
+      }
+    </style>
+    <textarea id\$="[[fieldId]]" name="[[fieldName]]" hidden="">[[bodyValue]]</textarea>
+    <cms-hax open-default="[[openDefault]]" hide-message="" body-offset-left="[[bodyOffsetLeft]]" update-page-data="[[updatePageData]]" end-point="[[endPoint]]" app-store-connection="[[appStoreConnection]]" hide-export-button="[[hideExportButton]]" align="[[align]]"></cms-hax>
+`,
+
+  is: "wysiwyg-hax",
+
+  properties: {
+    /**
+     * Default the panel to open
+     */
+    openDefault: {
+      type: Boolean,
+      value: false
+    },
+    /**
+     * Hide the export button, not a common thing to show
+     * in this mode but it's possible for debugging
+     */
+    hideExportButton: {
+      type: Boolean,
+      value: true
+    },
+    /**
+     * Direction to align the hax edit panel
+     */
+    align: {
+      type: String,
+      value: "right"
+    },
+    /**
+     * Data binding of a hidden text area with the value from the hax-body tag
+     */
+    bodyValue: {
+      type: String
+    },
+    /**
+     * Connection object for talking to an app store.
+     */
+    appStoreConnection: {
+      type: Object
+    },
+    /**
+     * fieldId, id value on the input field.
+     */
+    fieldId: {
+      type: String,
+      value: "textarea-input-field"
+    },
+    /**
+     * fieldName, internal to the form in whatever system it's in.
+     */
+    fieldName: {
+      type: String,
+      value: "data[content]"
+    },
+    /**
+     * Offset from the left of the body field
+     */
+    bodyOffsetLeft: {
+      type: Number,
+      value: -22
+    },
+    /**
+     * State of the panel
+     */
+    editMode: {
+      type: Boolean,
+      reflectToAttribute: true
+    },
+    /**
+     * Location to save content to.
+     */
+    endPoint: {
+      type: String
+    },
+    /**
+     * Page data, body of text as a string.
+     */
+    updatePageData: {
+      type: String
+    },
+    /**
+     * Reference to activeBody.
+     */
+    activeHaxBody: {
+      type: Object,
+      observer: "_activeHaxBodyUpdated"
+    },
+    __imported: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * Ensure we've imported our content on initial setup
+   */
+  _activeHaxBodyUpdated: function(newValue, oldValue) {
+    // ensure we import our content once we get an initial registration of active body
+    if (newValue != null && !this.__imported) {
+      this.__imported = true;
+      // see what's inside of this, in a template tag
+      let children = this.queryEffectiveChildren("template");
+      // convert this template content into the real thing
+      // this helps with correctly preserving everything on the way down
+      if (typeof children !== typeof undefined) {
+        newValue.importContent(children.innerHTML);
+        // need to dot his because of juggling unfortunately
+        this.editMode = false;
+        Polymer.HaxStore.write("editMode", this.editMode, this);
+        setTimeout(() => {
+          this.editMode = true;
+          Polymer.HaxStore.write("editMode", this.editMode, this);
+        }, 200);
+      }
+    }
+  },
+
+  /**
+   * Created life cycle
+   */
+  created: function() {
+    document.body.addEventListener(
+      "hax-store-property-updated",
+      this._haxStorePropertyUpdated.bind(this)
+    );
+  },
+
+  /**
+   * Attached to the DOM; now we can fire event to the store that
+   * we exist and are the thing being edited.
+   */
+  attached: function() {
+    document.body.addEventListener(
+      "hax-save",
+      this._bodyContentUpdated.bind(this)
+    );
+    document.body.addEventListener(
+      "hax-store-property-updated",
+      this._haxStorePropertyUpdated.bind(this)
+    );
+    // import the text area down into the active hax body, which cms-hax is providing
+  },
+
+  /**
+   * Store updated, sync.
+   */
+  _haxStorePropertyUpdated: function(e) {
+    if (
+      e.detail &&
+      typeof e.detail.value !== typeof undefined &&
+      e.detail.property
+    ) {
+      if (typeof e.detail.value === "object") {
+        this.set(e.detail.property, null);
+      }
+      this.set(e.detail.property, e.detail.value);
+    }
+  },
+
+  /**
+   * Set the bubbled up event to the body value that just got changed
+   */
+  _bodyContentUpdated: function(e) {
+    this.bodyValue = Polymer.HaxStore.instance.activeHaxBody.haxToContent();
+  }
+});
