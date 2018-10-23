4bf78ca3ecacc48cf1c2d0cf4cbd13af7e200229
btopro <btopro@outlook.com>
2018-10-23 16:54:28 -0400

    automated modulizer starting point for simple-concept-network/

---
+++ elements/simple-concept-network/src/simple-concept-network.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { SimpleConceptNetwork };
+/**
+ * `simple-concept-network`
+ * `Automated conversion of simple-concept-network/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class SimpleConceptNetwork extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "simple-concept-network";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      SimpleConceptNetwork.haxProperties,
+      SimpleConceptNetwork.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(SimpleConceptNetwork.tag, SimpleConceptNetwork);
