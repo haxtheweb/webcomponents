d96bdb5d8595c47c57e0089fe6a7bf01388722f9
btopro <btopro@outlook.com>
2018-10-23 14:29:24 -0400

    automated modulizer starting point for hax-editable/

---
+++ elements/hax-editable/hax-editable.es6.js
@@ -0,0 +1,10 @@
+import{html,PolymerElement}from"./node_modules/@polymer/polymer/polymer-element.js";import{HAXWiring}from"./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";export{HaxEditable};class HaxEditable extends PolymerElement{static get template(){return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get haxProperties(){return{canScale:!0,canPosition:!0,canEditSource:!1,gizmo:{title:"Hax editable",description:"Automated conversion of hax-editable/",icon:"icons:android",color:"green",groups:["Editable"],handles:[{type:"todo:read-the-docs-for-usage"}],meta:{author:"btopro",owner:"The Pennsylvania State University"}},settings:{quick:[],configure:[],advanced:[]}}}static get properties(){return{}}static get tag(){return"hax-editable"}connectedCallback(){super.connectedCallback();this.HAXWiring=new HAXWiring;this.HAXWiring.setHaxProperties(HaxEditable.haxProperties,HaxEditable.tag,this)}}window.customElements.define(HaxEditable.tag,HaxEditable);
