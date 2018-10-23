10fb2f994bbf28a8e22731fa203855303362e3ce
btopro <btopro@outlook.com>
2018-10-23 15:25:34 -0400

    automated modulizer starting point for lrndesign-imagecarousel/

---
+++ elements/lrndesign-imagecarousel/build/es6/lrndesign-imagecarousel.js
@@ -0,0 +1,56 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrndesignImagecarousel };
+class LrndesignImagecarousel extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Lrndesign imagecarousel",
+        description: "Automated conversion of lrndesign-imagecarousel/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Imagecarousel"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "lrndesign-imagecarousel";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      LrndesignImagecarousel.haxProperties,
+      LrndesignImagecarousel.tag,
+      this
+    );
+  }
+}
+window.customElements.define(
+  LrndesignImagecarousel.tag,
+  LrndesignImagecarousel
+);
