805eb27b0c7992d9acab656b46063c0b33d11666
btopro <btopro@outlook.com>
2018-10-23 15:26:26 -0400

    automated modulizer starting point for lrndesign-imagemap/

---
+++ elements/lrndesign-imagemap/lib/lrndesign-imagemap-hotspot.js
@@ -0,0 +1,54 @@
+import "@polymer/polymer/polymer.js";
+import "@polymer/iron-a11y-keys/iron-a11y-keys.js";
+import "relative-heading/relative-heading.js";
+/**
+ * `lrndesign-imagemap-hotspot`
+ * creates an accessible image map
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+Polymer({
+  _template: `
+    <style>
+      :host {
+        display: none;
+      }
+      :host #desc {
+        margin: 0 0 15px;
+      }
+      @media print {
+        :host {
+          display: block;
+        }
+      }
+    </style>
+    <relative-heading hidden\$="[[!label]]" id="heading" text\$="[[label]]">
+    </relative-heading>
+    <div id="desc"><slot></slot></div>
+`,
+
+  is: "lrndesign-imagemap-hotspot",
+
+  properties: {
+    /**
+     * Label for the hotspot
+     */
+    label: {
+      type: String,
+      value: null
+    },
+    /**
+     * Id of hotspot element inside the SVG
+     */
+    hotspotId: {
+      type: String,
+      value: null
+    }
+  },
+
+  setParentHeading: function(parent) {
+    this.$.heading._setParent(parent);
+  }
+});
---
+++ elements/lrndesign-imagemap/lrndesign-imagemap.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrndesignImagemap };
+/**
+ * `lrndesign-imagemap`
+ * `Automated conversion of lrndesign-imagemap/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class LrndesignImagemap extends PolymerElement {
+  // render function
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+
+  // haxProperty definition
+  static get haxProperties() {
+    return {
+      canScale: true,
+      canPosition: true,
+      canEditSource: false,
+      gizmo: {
+        title: "Lrndesign imagemap",
+        description: "Automated conversion of lrndesign-imagemap/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Imagemap"],
+        handles: [
+          {
+            type: "todo:read-the-docs-for-usage"
+          }
+        ],
+        meta: {
+          author: "btopro",
+          owner: "The Pennsylvania State University"
+        }
+      },
+      settings: {
+        quick: [],
+        configure: [],
+        advanced: []
+      }
+    };
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {};
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "lrndesign-imagemap";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      LrndesignImagemap.haxProperties,
+      LrndesignImagemap.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(LrndesignImagemap.tag, LrndesignImagemap);
