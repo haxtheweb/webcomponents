26b3fa9a4fb0afabb88f005d1fb736acccd6bacd
btopro <btopro@outlook.com>
2018-10-23 15:09:27 -0400

    automated modulizer starting point for lrndesign-blockquote/

---
+++ elements/lrndesign-blockquote/build/es6/lrndesign-blockquote.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrndesignBlockquote };
+class LrndesignBlockquote extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Lrndesign blockquote",
+        description: "Automated conversion of lrndesign-blockquote/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Blockquote"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "lrndesign-blockquote";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      LrndesignBlockquote.haxProperties,
+      LrndesignBlockquote.tag,
+      this
+    );
+  }
+}
+window.customElements.define(LrndesignBlockquote.tag, LrndesignBlockquote);
