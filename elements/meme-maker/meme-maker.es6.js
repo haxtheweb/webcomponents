58f2527fff9e59a0ef138123496641096f482673
btopro <btopro@outlook.com>
2018-10-23 16:11:06 -0400

    automated modulizer starting point for meme-maker/

---
+++ elements/meme-maker/meme-maker.es6.js
@@ -0,0 +1,10 @@
+import{html,PolymerElement}from"./node_modules/@polymer/polymer/polymer-element.js";import{HAXWiring}from"./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";export{MemeMaker};class MemeMaker extends PolymerElement{static get template(){return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get haxProperties(){return{canScale:!0,canPosition:!0,canEditSource:!1,gizmo:{title:"Meme maker",description:"Automated conversion of meme-maker/",icon:"icons:android",color:"green",groups:["Maker"],handles:[{type:"todo:read-the-docs-for-usage"}],meta:{author:"btopro",owner:"The Pennsylvania State University"}},settings:{quick:[],configure:[],advanced:[]}}}static get properties(){return{}}static get tag(){return"meme-maker"}connectedCallback(){super.connectedCallback();this.HAXWiring=new HAXWiring;this.HAXWiring.setHaxProperties(MemeMaker.haxProperties,MemeMaker.tag,this)}}window.customElements.define(MemeMaker.tag,MemeMaker);
