610d5b2b677d767be321dff59cad6460c1c512a9
btopro <btopro@outlook.com>
2018-10-23 14:42:32 -0400

    automated modulizer starting point for lrn-assignment/

---
+++ elements/lrn-assignment/build/es6/lrn-assignment.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrnAssignment };
+class LrnAssignment extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Lrn assignment",
+        description: "Automated conversion of lrn-assignment/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Assignment"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "lrn-assignment";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      LrnAssignment.haxProperties,
+      LrnAssignment.tag,
+      this
+    );
+  }
+}
+window.customElements.define(LrnAssignment.tag, LrnAssignment);
