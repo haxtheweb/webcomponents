5f9ba471d0a92b0564bb197872c455e4f532039b
btopro <btopro@outlook.com>
2018-10-23 16:18:02 -0400

    automated modulizer starting point for page-scroll-position/

---
+++ elements/page-scroll-position/src/page-scroll-position.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { PageScrollPosition };
+/**
+ * `page-scroll-position`
+ * `Automated conversion of page-scroll-position/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class PageScrollPosition extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "page-scroll-position";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      PageScrollPosition.haxProperties,
+      PageScrollPosition.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(PageScrollPosition.tag, PageScrollPosition);
