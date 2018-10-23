0498fd2c7274de16f31a17bc45ae751ab8cf77b8
btopro <btopro@outlook.com>
2018-10-23 16:38:19 -0400

    automated modulizer starting point for polymer-training/

---
+++ elements/polymer-training/polymer-training.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { PolymerTraining };
+/**
+ * `polymer-training`
+ * `Automated conversion of polymer-training/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class PolymerTraining extends PolymerElement {
+  // render function
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+
+  // haxProperty definition
+  static get haxProperties() {
+    return {
+      canScale: true,
+      canPosition: true,
+      canEditSource: false,
+      gizmo: {
+        title: "Polymer training",
+        description: "Automated conversion of polymer-training/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Training"],
+        handles: [
+          {
+            type: "todo:read-the-docs-for-usage"
+          }
+        ],
+        meta: {
+          author: "btopro",
+          owner: "The Pennsylvania State University"
+        }
+      },
+      settings: {
+        quick: [],
+        configure: [],
+        advanced: []
+      }
+    };
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {};
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "polymer-training";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      PolymerTraining.haxProperties,
+      PolymerTraining.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(PolymerTraining.tag, PolymerTraining);
