a7c5136f6603c18c1c618eb02fd81b19f0d22d8d
btopro <btopro@outlook.com>
2018-10-23 14:38:42 -0400

    automated modulizer starting point for json-outline-schema/

---
+++ elements/json-outline-schema/build/es6/json-outline-schema.js
@@ -0,0 +1,53 @@
+import {
+  html,
+  PolymerElement
+} from "./node_modules/@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "./node_modules/@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { JsonOutlineSchema };
+class JsonOutlineSchema extends PolymerElement {
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+  static get haxProperties() {
+    return {
+      canScale: !0,
+      canPosition: !0,
+      canEditSource: !1,
+      gizmo: {
+        title: "Json outline-schema",
+        description: "Automated conversion of json-outline-schema/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Outline"],
+        handles: [{ type: "todo:read-the-docs-for-usage" }],
+        meta: { author: "btopro", owner: "The Pennsylvania State University" }
+      },
+      settings: { quick: [], configure: [], advanced: [] }
+    };
+  }
+  static get properties() {
+    return {};
+  }
+  static get tag() {
+    return "json-outline-schema";
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      JsonOutlineSchema.haxProperties,
+      JsonOutlineSchema.tag,
+      this
+    );
+  }
+}
+window.customElements.define(JsonOutlineSchema.tag, JsonOutlineSchema);
