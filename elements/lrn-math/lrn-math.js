8e51139557d2d2870870431dbb6c1eac8158d70f
btopro <btopro@outlook.com>
2018-10-23 14:51:24 -0400

    automated modulizer starting point for lrn-math/

---
+++ elements/lrn-math/lrn-math.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { LrnMath };
+/**
+ * `lrn-math`
+ * `Automated conversion of lrn-math/`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class LrnMath extends PolymerElement {
+  // render function
+  static get template() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`;
+  }
+
+  // haxProperty definition
+  static get haxProperties() {
+    return {
+      canScale: true,
+      canPosition: true,
+      canEditSource: false,
+      gizmo: {
+        title: "Lrn math",
+        description: "Automated conversion of lrn-math/",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Math"],
+        handles: [
+          {
+            type: "todo:read-the-docs-for-usage"
+          }
+        ],
+        meta: {
+          author: "btopro",
+          owner: "The Pennsylvania State University"
+        }
+      },
+      settings: {
+        quick: [],
+        configure: [],
+        advanced: []
+      }
+    };
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {};
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "lrn-math";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(LrnMath.haxProperties, LrnMath.tag, this);
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(LrnMath.tag, LrnMath);
