44b0ac319921faeaa36da11fc9c3d41ea9fac9af
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2018-10-31 12:18:33 -0400

    Refactoring drawing-icons to match iron-icons

--- elements/drawing-icons/src/drawing-icons.js
+++ elements/drawing-icons/src/drawing-icons.js
@@ -2,39 +2,65 @@
  * Copyright 2018 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
-import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-iconset-svg/iron-iconset-svg.js";
 
-export { DrawingIcons };
-/**
- * `drawing-icons`
- * `iconset for drawing toolbar buttons`
- *
- * @microcopy - language worth noting:
- *  -
- *
- * @customElement
- * @polymer
- * @demo demo/index.html
- */
-class DrawingIcons extends PolymerElement {
-  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+import { html } from "@polymer/polymer/lib/utils/html-tag.js";
 
-  /**
-   * Store the tag name to make it easier to obtain directly.
-   * @notice function name must be here for tooling to operate correctly
-   */
-  static get tag() {
-    return "drawing-icons";
-  }
-  /**
-   * life cycle, element is afixed to the DOM
-   */
-  connectedCallback() {
-    super.connectedCallback();
-  }
-  /**
-   * life cycle, element is removed from the DOM
-   */
-  //disconnectedCallback() {}
-}
-window.customElements.define(DrawingIcons.tag, DrawingIcons);
+const template = html`<iron-iconset-svg size="24" name="drawing">
+  <!-- move -->
+  <svg>
+    <defs>
+      <g id="move">
+        <polygon points="21,12 17,8 17,11 13,11 13,7 16,7 12,3 8,7 11,7 11,11 7,11 7,8 3,12 7,16 7,13 11,13 11,17 8,17 12,21 16,17 
+          13,17 13,13 17,13 17,16 	"></polygon>
+      </g>
+    </defs>
+  </svg>
+  
+  
+  <!-- select -->
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <defs>
+      <g id="select">
+        <path d="M6.3 2.8l.3 19.8.9.4 5.1-5.4 7.4.1.4-.9-14.1-14z"></path>
+      </g>
+    </defs>
+  </svg>
+
+
+  <!-- draw ellipsis -->
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <defs>
+      <g id="draw-ellip">
+        <ellipse cx="12" cy="11.9" rx="10.1" ry="7.1" opacity=".3" stroke-width="2" stroke-miterlimit="10"></ellipse>
+        <ellipse cx="12" cy="11.9" rx="10.1" ry="7.1" stroke="#000" fill="none" stroke-width="2" stroke-miterlimit="10"></ellipse>
+      </g>
+    </defs>
+  </svg>
+
+
+  <!-- draw rectangle -->
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <defs>
+      <g id="draw-rect">
+        <path opacity=".3" d="M3.1 6h18v12.1h-18z" stroke-width="2" stroke-miterlimit="10"></path>
+        <path fill="none" d="M3.1 6h18v12.1h-18z" stroke="#000" stroke-width="2" stroke-miterlimit="10"></path>
+      </g>
+    </defs>
+  </svg>
+  
+  
+  <!-- draw polygon -->
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <defs>
+      <g id="draw-poly">
+        <path opacity=".3" d="M2.7 7.3l17.6-2.8-4.1 16.6-11.6-5z" stroke-width="2" stroke-miterlimit="10"></path>
+        <path fill="none" stroke="#000" stroke-width="2" stroke-miterlimit="10" d="M2.7 7.3l17.6-2.8-4.1 16.6-11.6-5z"></path>
+      </g>
+    </defs>
+  </svg>
+  
+</iron-iconset-svg>`;
+
+document.head.appendChild(template.content);
