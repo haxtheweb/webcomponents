f4d30a3882edbcc854e069f0ebebe69eccee3c8d
btopro <btopro@outlook.com>
2018-11-21 11:10:44 -0500

    example-hax-element added as a vanillaJS element #103

---
+++ elements/example-hax-element/src/example-hax-element.js
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { ExampleHaxElement };
+/**
+ * `example-hax-element`
+ * `Provide an example to pick apart of a working HAX element`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @demo demo/index.html
+ */
+class ExampleHaxElement extends HTMLElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "example-hax-element";
+  }
+  /**
+   * life cycle
+   */
+  constructor(delayRender = false) {
+    super();
+
+    // set tag for later use
+    this.tag = ExampleHaxElement.tag;
+    // map our imported properties json to real props on the element
+    // @notice static getter of properties is built via tooling
+    // to edit modify src/ExampleHaxElement-properties.json
+    let obj = ExampleHaxElement.properties;
+    for (let p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        if (this.hasAttribute(p)) {
+          this[p] = this.getAttribute(p);
+        } else {
+          this.setAttribute(p, obj[p].value);
+          this[p] = obj[p].value;
+        }
+      }
+    }
+    // optional queue for future use
+    this._queue = [];
+    this.template = document.createElement("template");
+
+    this.attachShadow({ mode: "open" });
+
+    if (!delayRender) {
+      this.render();
+    }
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    if (window.ShadyCSS) {
+      window.ShadyCSS.styleElement(this);
+    }
+
+    if (this._queue.length) {
+      this._processQueue();
+    }
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      ExampleHaxElement.haxProperties,
+      ExampleHaxElement.tag,
+      this
+    );
+  }
+
+  _copyAttribute(name, to) {
+    const recipients = this.shadowRoot.querySelectorAll(to);
+    const value = this.getAttribute(name);
+    const fname = value == null ? "removeAttribute" : "setAttribute";
+    for (const node of recipients) {
+      node[fname](name, value);
+    }
+  }
+
+  _queueAction(action) {
+    this._queue.push(action);
+  }
+
+  _processQueue() {
+    this._queue.forEach(action => {
+      this[`_${action.type}`](action.data);
+    });
+
+    this._queue = [];
+  }
+
+  _setProperty({ name, value }) {
+    this[name] = value;
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = null;
+    this.template.innerHTML = this.html;
+
+    if (window.ShadyCSS) {
+      window.ShadyCSS.prepareTemplate(this.template, this.tag);
+    }
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+  }
+
+  //static get observedAttributes() {
+  //  return [];
+  //}
+  // disconnectedCallback() {}
+  // attributeChangedCallback(attr, oldValue, newValue) {}
+}
+window.customElements.define(ExampleHaxElement.tag, ExampleHaxElement);
