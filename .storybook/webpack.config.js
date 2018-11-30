b2ee85c83b49e8770314a7fe7623892d38787595
btopro <btopro@outlook.com>
2018-11-30 15:54:36 -0500

    attempt at dynamic import fix but our repo is massive so..

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,27 +1,7 @@
 const path = require("path");
 module.exports = (baseConfig, env, defaultConfig) => {
-  // Make whatever fine-grained changes you need
-  defaultConfig.resolve.alias = {
-    "../../whatwg-fetch/fetch.js": path.join(
-      __dirname,
-      "../node_modules/whatwg-fetch/fetch.js"
-    ),
-    "../../numeral/min/numeral.min.js": path.join(
-      __dirname,
-      "../node_modules/numeral/min/numeral.min.js"
-    ),
-    "../../numeral/numeral.js": path.join(
-      __dirname,
-      "../node_modules/numeral/numeral.js"
-    ),
-    "../../dialog-polyfill/dialog-polyfill.js": path.join(
-      __dirname,
-      "../node_modules/dialog-polyfill/dialog-polyfill.js"
-    )
-  };
   defaultConfig.module.rules.push({
-    exclude: /lib\/.*\/.*.js/,
-    test: /elements\/.*\/.*.js$/,
+    test: [/\.js$/],
     loader: require.resolve(
       "@open-wc/webpack/loaders/import-meta-url-loader.js"
     )
