c53250a532b61261c6022f4deceb5f93a0528c10
btopro <btopro@outlook.com>
2018-12-04 13:22:45 -0500

    baseline-build of hax for #127

---
+++ elements/baseline-build-hax/baseline-build-hax.es6.js
@@ -0,0 +1,10 @@
+class BaselineBuildHax extends HTMLElement{get html(){return`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+</style>
+<slot></slot>`}static get properties(){return{}}static get tag(){return"baseline-build-hax"}constructor(delayRender=!1){super();this.tag=BaselineBuildHax.tag;let obj=BaselineBuildHax.properties;for(let p in obj){if(obj.hasOwnProperty(p)){if(this.hasAttribute(p)){this[p]=this.getAttribute(p)}else{this.setAttribute(p,obj[p].value);this[p]=obj[p].value}}}this._queue=[];this.template=document.createElement("template");this.attachShadow({mode:"open"});if(!delayRender){this.render()}}connectedCallback(){if(window.ShadyCSS){window.ShadyCSS.styleElement(this)}if(this._queue.length){this._processQueue()}}_copyAttribute(name,to){const recipients=this.shadowRoot.querySelectorAll(to),value=this.getAttribute(name),fname=null==value?"removeAttribute":"setAttribute";for(const node of recipients){node[fname](name,value)}}_queueAction(action){this._queue.push(action)}_processQueue(){this._queue.forEach(action=>{this[`_${action.type}`](action.data)});this._queue=[]}_setProperty({name,value}){this[name]=value}render(){this.shadowRoot.innerHTML=null;this.template.innerHTML=this.html;if(window.ShadyCSS){window.ShadyCSS.prepareTemplate(this.template,this.tag)}this.shadowRoot.appendChild(this.template.content.cloneNode(!0))}}window.customElements.define(BaselineBuildHax.tag,BaselineBuildHax);export{BaselineBuildHax};
---
+++ elements/baseline-build-hax/src/baseline-build-hax.js
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+
+/**
+ * `baseline-build-hax`
+ * `create an inflexible baseline build of hax with all elements`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @demo demo/index.html
+ */
+class BaselineBuildHax extends HTMLElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "baseline-build-hax";
+  }
+  /**
+   * life cycle
+   */
+  constructor(delayRender = false) {
+    super();
+
+    // set tag for later use
+    this.tag = BaselineBuildHax.tag;
+    // map our imported properties json to real props on the element
+    // @notice static getter of properties is built via tooling
+    // to edit modify src/BaselineBuildHax-properties.json
+    let obj = BaselineBuildHax.properties;
+    for (let p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        if (this.hasAttribute(p)) {
+          this[p] = this.getAttribute(p);
+        } else {
+          this.setAttribute(p, obj[p].value);
+          this[p] = obj[p].value;
+        }
+      }
+    }
+    // optional queue for future use
+    this._queue = [];
+    this.template = document.createElement("template");
+
+    this.attachShadow({ mode: "open" });
+
+    if (!delayRender) {
+      this.render();
+    }
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    if (window.ShadyCSS) {
+      window.ShadyCSS.styleElement(this);
+    }
+
+    if (this._queue.length) {
+      this._processQueue();
+    }
+  }
+
+  _copyAttribute(name, to) {
+    const recipients = this.shadowRoot.querySelectorAll(to);
+    const value = this.getAttribute(name);
+    const fname = value == null ? "removeAttribute" : "setAttribute";
+    for (const node of recipients) {
+      node[fname](name, value);
+    }
+  }
+
+  _queueAction(action) {
+    this._queue.push(action);
+  }
+
+  _processQueue() {
+    this._queue.forEach(action => {
+      this[`_${action.type}`](action.data);
+    });
+
+    this._queue = [];
+  }
+
+  _setProperty({ name, value }) {
+    this[name] = value;
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = null;
+    this.template.innerHTML = this.html;
+
+    if (window.ShadyCSS) {
+      window.ShadyCSS.prepareTemplate(this.template, this.tag);
+    }
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+  }
+
+  //static get observedAttributes() {
+  //  return [];
+  //}
+  // disconnectedCallback() {}
+  // attributeChangedCallback(attr, oldValue, newValue) {}
+}
+window.customElements.define(BaselineBuildHax.tag, BaselineBuildHax);
+export { BaselineBuildHax };
