f3319dc63a357b4faf4db0744cf8c15992f7350c
btopro <btopro@outlook.com>
2018-12-04 13:35:52 -0500

    added more dependencies

--- elements/baseline-build-hax/baseline-build-hax.js
+++ elements/baseline-build-hax/baseline-build-hax.js
@@ -2,129 +2,58 @@
  * Copyright 2018 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
-
-/**
- * `baseline-build-hax`
- * `create an inflexible baseline build of hax with all elements`
- *
- * @microcopy - language worth noting:
- *  -
- *
- * @customElement
- * @demo demo/index.html
- */
-class BaselineBuildHax extends HTMLElement {
-  // render function
-  get html() {
-    return `
-<style>:host {
-  display: block;
-}
-
-:host([hidden]) {
-  display: none;
-}
-</style>
-<slot></slot>`;
-  }
-
-  // properties available to the custom element for data binding
-  static get properties() {
-    return {};
-  }
-
-  /**
-   * Store the tag name to make it easier to obtain directly.
-   * @notice function name must be here for tooling to operate correctly
-   */
-  static get tag() {
-    return "baseline-build-hax";
-  }
-  /**
-   * life cycle
-   */
-  constructor(delayRender = false) {
-    super();
-
-    // set tag for later use
-    this.tag = BaselineBuildHax.tag;
-    // map our imported properties json to real props on the element
-    // @notice static getter of properties is built via tooling
-    // to edit modify src/BaselineBuildHax-properties.json
-    let obj = BaselineBuildHax.properties;
-    for (let p in obj) {
-      if (obj.hasOwnProperty(p)) {
-        if (this.hasAttribute(p)) {
-          this[p] = this.getAttribute(p);
-        } else {
-          this.setAttribute(p, obj[p].value);
-          this[p] = obj[p].value;
-        }
-      }
-    }
-    // optional queue for future use
-    this._queue = [];
-    this.template = document.createElement("template");
-
-    this.attachShadow({ mode: "open" });
-
-    if (!delayRender) {
-      this.render();
-    }
-  }
-  /**
-   * life cycle, element is afixed to the DOM
-   */
-  connectedCallback() {
-    if (window.ShadyCSS) {
-      window.ShadyCSS.styleElement(this);
-    }
-
-    if (this._queue.length) {
-      this._processQueue();
-    }
-  }
-
-  _copyAttribute(name, to) {
-    const recipients = this.shadowRoot.querySelectorAll(to);
-    const value = this.getAttribute(name);
-    const fname = value == null ? "removeAttribute" : "setAttribute";
-    for (const node of recipients) {
-      node[fname](name, value);
-    }
-  }
-
-  _queueAction(action) {
-    this._queue.push(action);
-  }
-
-  _processQueue() {
-    this._queue.forEach(action => {
-      this[`_${action.type}`](action.data);
-    });
-
-    this._queue = [];
-  }
-
-  _setProperty({ name, value }) {
-    this[name] = value;
-  }
-
-  render() {
-    this.shadowRoot.innerHTML = null;
-    this.template.innerHTML = this.html;
-
-    if (window.ShadyCSS) {
-      window.ShadyCSS.prepareTemplate(this.template, this.tag);
-    }
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
-  }
-
-  //static get observedAttributes() {
-  //  return [];
-  //}
-  // disconnectedCallback() {}
-  // attributeChangedCallback(attr, oldValue, newValue) {}
-}
-window.customElements.define(BaselineBuildHax.tag, BaselineBuildHax);
-export { BaselineBuildHax };
+import "@polymer/iron-icons/iron-icons.js";
+import "@polymer/iron-icons/editor-icons.js";
+import "@polymer/iron-icons/device-icons.js";
+import "@polymer/iron-icons/hardware-icons.js";
+import "@polymer/iron-icons/communication-icons.js";
+import "@polymer/iron-icons/social-icons.js";
+import "@polymer/iron-icons/av-icons.js";
+import "@polymer/iron-icons/places-icons.js";
+import "@polymer/iron-icons/maps-icons.js";
+import "@polymer/iron-image/iron-image.js";
+import "@lrnwebcomponents/lrn-icons/lrn-icons.js";
+import "@lrnwebcomponents/wysiwyg-hax/wysiwyg-hax.js";
+import "@lrnwebcomponents/cms-hax/cms-hax.js";
+import "@lrnwebcomponents/hax-body/hax-body.js";
+import "@lrnwebcomponents/hax-body/lib/hax-store.js";
+import "@lrnwebcomponents/hax-body/lib/hax-autoloader.js";
+import "@lrnwebcomponents/hax-body/lib/hax-manager.js";
+import "@lrnwebcomponents/hax-body/lib/hax-app-picker.js";
+import "@lrnwebcomponents/hax-body/lib/hax-app.js";
+import "@lrnwebcomponents/hax-body/lib/hax-panel.js";
+import "@lrnwebcomponents/hax-body/lib/hax-export-dialog.js";
+import "@lrnwebcomponents/hax-body/lib/hax-toolbar.js";
+import "@lrnwebcomponents/a11y-gif-player/a11y-gif-player.js";
+import "@lrnwebcomponents/aframe-player/aframe-player.js";
+import "@lrnwebcomponents/citation-element/citation-element.js";
+import "@lrnwebcomponents/deduping-fix/deduping-fix.js";
+import "@lrnwebcomponents/hero-banner/hero-banner.js";
+import "@lrnwebcomponents/image-compare-slider/image-compare-slider.js";
+import "@lrnwebcomponents/license-element/license-element.js";
+import "@lrnwebcomponents/lrn-aside/lrn-aside.js";
+import "@lrnwebcomponents/lrn-calendar/lrn-calendar.js";
+import "@lrnwebcomponents/lrn-math/lrn-math.js";
+import "@lrnwebcomponents/lrn-table/lrn-table.js";
+import "@lrnwebcomponents/lrn-vocab/lrn-vocab.js";
+import "@lrnwebcomponents/lrndesign-blockquote/lrndesign-blockquote.js";
+import "@lrnwebcomponents/magazine-cover/magazine-cover.js";
+import "@lrnwebcomponents/materializecss-styles/materializecss-styles.js";
+import "@lrnwebcomponents/media-behaviors/media-behaviors.js";
+import "@lrnwebcomponents/media-image/media-image.js";
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "@lrnwebcomponents/multiple-choice/multiple-choice.js";
+import "@lrnwebcomponents/paper-audio-player/paper-audio-player.js";
+import "@lrnwebcomponents/pdf-element/pdf-element.js";
+import "@lrnwebcomponents/person-testimonial/person-testimonial.js";
+import "@lrnwebcomponents/place-holder/place-holder.js";
+import "@lrnwebcomponents/q-r/q-r.js";
+import "@lrnwebcomponents/self-check/self-check.js";
+import "@lrnwebcomponents/simple-colors/simple-colors.js";
+import "@lrnwebcomponents/simple-concept-network/simple-concept-network.js";
+import "@lrnwebcomponents/stop-note/stop-note.js";
+import "@lrnwebcomponents/tab-list/tab-list.js";
+import "@lrnwebcomponents/task-list/task-list.js";
+import "@lrnwebcomponents/video-player/video-player.js";
+import "@lrnwebcomponents/wave-player/wave-player.js";
+import "@lrnwebcomponents/wikipedia-query/wikipedia-query.js";
