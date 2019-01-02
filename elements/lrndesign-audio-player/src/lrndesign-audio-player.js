dfc1f8677b7204df1553ac819819e812d16d1c29
btopro <btopro@outlook.com>
2019-01-02 10:55:36 -0500

    license inclusion requested by author this was forked from

--- elements/lrndesign-audio-player/src/lrndesign-audio-player.js
+++ elements/lrndesign-audio-player/src/lrndesign-audio-player.js
@@ -6,6 +6,28 @@ import "@polymer/paper-ripple/paper-ripple.js";
 import { IronA11yKeysBehavior } from "@polymer/iron-a11y-keys-behavior/iron-a11y-keys-behavior.js";
 import "./lib/lrndesign-audio-player-icons.js";
 /**
+ * The MIT License (MIT)
+
+Copyright (c) 2015 Nadi Dikun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 A custom audio player with material paper style and clean design.
 
 Example:
