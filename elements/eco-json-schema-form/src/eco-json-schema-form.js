aec261759bc39a01084e67daeb21f5d43379fcc7
btopro <btopro@outlook.com>
2019-01-08 08:39:02 -0500

    minor licensing notice on eco-json-schema to credit original author

--- elements/eco-json-schema-form/src/eco-json-schema-form.js
+++ elements/eco-json-schema-form/src/eco-json-schema-form.js
@@ -1,5 +1,9 @@
 /**
- * Copyright 2018 The Pennsylvania State University
- * @license Apache-2.0, see License.md for full text.
+ * This draws heavily upon the work of
+ * https://github.com/DimShadoWWW/eco-json-schema-form which is a fork of
+ * https://github.com/ecoutu/eco-json-schema-form
+ *
+ * This has theme, style, data binding and compilation enhancements
+ * beyond the originals it has stemmed from.
  */
 import "./lib/eco-json-schema-wizard.js";
