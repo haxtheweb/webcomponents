cfd969a4aaeaaa4ad4d65e882f5725b5fd985ed3
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2019-01-22 08:45:50 -0500

    init timeline-tool

---
+++ elements/timeline-tool/src/timeline-tool.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+export { TimelineTool };
+/**
+ * `timeline-tool`
+ * `Makes a HAX-wired timeline`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class TimelineTool extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "timeline-tool";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setHaxProperties(
+      TimelineTool.haxProperties,
+      TimelineTool.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(TimelineTool.tag, TimelineTool);
