283085f633ae54fc92f77b5e0d211af5bef89d7b
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2019-02-04 12:45:57 -0500

    Init rich text editor

---
+++ elements/rich-text-editor/rich-text-editor.js
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+/**
+ * `rich-text-editor`
+ * `a standalone rich text editor`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class RichTextEditor extends PolymerElement {
+  // render function
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+
+        :host([hidden]) {
+          display: none;
+        }
+      </style>
+      <label>[[label]]</label>
+      <slot></slot>
+    `;
+  }
+
+  // haxProperty definition
+  static get haxProperties() {
+    return {
+      canScale: true,
+      canPosition: true,
+      canEditSource: false,
+      gizmo: {
+        title: "Rich text-editor",
+        description: "a standalone rich text editor",
+        icon: "icons:android",
+        color: "green",
+        groups: ["Text"],
+        handles: [
+          {
+            type: "todo:read-the-docs-for-usage"
+          }
+        ],
+        meta: {
+          author: "nikkimk",
+          owner: "Penn State University"
+        }
+      },
+      settings: {
+        quick: [],
+        configure: [
+          {
+            property: "title",
+            description: "",
+            inputMethod: "textfield",
+            required: false,
+            icon: "icons:android"
+          }
+        ],
+        advanced: []
+      }
+    };
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {
+      label: {
+        name: "label",
+        type: "String",
+        value: '"Rich Text Editor"',
+        reflectToAttribute: true,
+        observer: false
+      }
+    };
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "rich-text-editor";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setup(
+      RichTextEditor.haxProperties,
+      RichTextEditor.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(RichTextEditor.tag, RichTextEditor);
+export { RichTextEditor };
---
+++ elements/rich-text-editor/src/rich-text-editor.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+/**
+ * `rich-text-editor`
+ * `a standalone rich text editor`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class RichTextEditor extends PolymerElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "rich-text-editor";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.HAXWiring = new HAXWiring();
+    this.HAXWiring.setup(
+      RichTextEditor.haxProperties,
+      RichTextEditor.tag,
+      this
+    );
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(RichTextEditor.tag, RichTextEditor);
+export { RichTextEditor };
