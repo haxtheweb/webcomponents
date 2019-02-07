6759d3bc668baa34969f063a3e66c5008354c2dd
heyMP <mgp140@gmail.com>
2019-02-07 10:37:04 -0500

    removed old state management code.

--- elements/haxcms-elements/lib/haxcms-site-router.js
+++ elements/haxcms-elements/lib/haxcms-site-router.js
@@ -42,6 +42,9 @@ Polymer({
    * ready life cycle
    */
   created: function() {
+    /**
+     * Subscribe to changes in the manifest
+     */
     autorun(() => {
       this._manifestChanged(store.routerManifest);
     });
@@ -49,20 +52,8 @@ Polymer({
       "vaadin-router-location-changed",
       this._routerLocationChanged.bind(this)
     );
-    // window.addEventListener(
-    //   "json-outline-schema-changed",
-    //   this._jsonOutlineSchemaChangedHandler.bind(this)
-    // );
-    // window.addEventListener(
-    //   "json-outline-schema-active-item-changed",
-    //   this._jsonOutlineSchemaActiveItemChangedHandler.bind(this)
-    // );
-    // window.addEventListener(
-    //   "haxcms-site-router-location-subscribe",
-    //   this._haxcmsSiteRouterLocationSubscribe.bind(this)
-    // );
   },
-  attached: function() {},
+
   /**
    * Detached life cycle
    */
@@ -71,18 +62,6 @@ Polymer({
       "vaadin-router-location-changed",
       this._routerLocationChanged.bind(this)
     );
-    // window.rem(
-    //   "json-outline-schema-changed",
-    //   this._jsonOutlineSchemaChangedHandler.bind(this)
-    // );
-    // window.addEventListener(
-    //   "json-outline-schema-active-item-changed",
-    //   this._jsonOutlineSchemaActiveItemChangedHandler.bind(this)
-    // );
-    // window.addEventListener(
-    //   "haxcms-site-router-location-subscribe",
-    //   this._haxcmsSiteRouterLocationSubscribe.bind(this)
-    // );
   },
 
   /**
@@ -126,77 +105,11 @@ Polymer({
 
   /**
    * React to page changes in the vaadin router and convert it
-   * to a "haxcms-active-item-changed" event.
+   * to a change in the mobx store.
    * @param {event} e
    */
   _routerLocationChanged: function(e) {
     //store local state
     store.location = e.detail.location;
-  },
-
-  /**
-   * Listen to changes to the root manifest update the local
-   * state.
-   * @param {event} e
-   */
-  _jsonOutlineSchemaChangedHandler: function(e) {
-    const manifest = e.detail;
-    this.manifest = {};
-    this.manifest = manifest;
-  },
-
-  /**
-   * White label JSON Outline Schema Event
-   *
-   * Subscribe to Active event changes.
-   *
-   * @param {event} e
-   */
-  _jsonOutlineSchemaActiveItemChangedHandler: function(e) {
-    window.dispatchEvent(
-      new CustomEvent("haxcms-site-router-active-item-changed", {
-        detail: e.detail
-      })
-    );
-  },
-
-  /**
-   * Event Handler for 'haxcms-router-location-changed-subscribe'
-   */
-  _haxcmsSiteRouterLocationSubscribe: function(e) {
-    const defaultOptions = {
-      setup: false
-    };
-    // combine default options and the subscription from the request
-    const subscription = Object.assign({}, defaultOptions, e.detail);
-    if (typeof subscription.callback === "undefined") {
-      console.error(
-        "No callback provided on haxcms-site-router-location-subscribe."
-      );
-      return;
-    }
-    if (typeof subscription.scope === "undefined") {
-      console.error(
-        "No scope provided on haxcms-site-router-location-subscribe."
-      );
-      return;
-    }
-    // dynaimcally add the event listener for more router manifest changes
-    subscription.scope.addEventListener(
-      "haxcms-site-router-location-changed",
-      subscription.scope[subscription.callback]
-    );
-    // if setup option is true then manually trigger the callback
-    if (subscription.setup) {
-      // create a synthetic event and send directly to the scope
-      const syntheticEvent = new CustomEvent(
-        "haxcms-site-router-location-changed",
-        {
-          detail: this._location
-        }
-      );
-      // manually call the change event
-      subscription.scope[subscription.callback](syntheticEvent);
-    }
   }
 });
