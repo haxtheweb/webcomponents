1e0cc3eb8a5f208a75744265b5091af725fe07f9
btopro <btopro@outlook.com>
2019-02-22 15:29:32 -0500

    icon picker removed log message

--- elements/simple-icon-picker/simple-icon-picker.js
+++ elements/simple-icon-picker/simple-icon-picker.js
@@ -209,7 +209,6 @@ class SimpleIconPicker extends SimplePicker {
    */
   _handleChange(e) {
     this.value = e.detail.value;
-    console.log("_handleChange", e.detail.value, this.value);
     this.dispatchEvent(
       new CustomEvent("change", { bubbles: true, detail: this })
     );
