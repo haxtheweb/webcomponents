6d3a9a8de636ef4a00b566085c9892071e11ff0d
btopro <btopro@outlook.com>
2019-03-11 15:14:17 -0400

    performance and a11y clean up

--- elements/map-menu/lib/map-menu-item.js
+++ elements/map-menu/lib/map-menu-item.js
@@ -7,35 +7,61 @@ Polymer({
     <style>
       :host {
         display: block;
+        transition: 0.2s all ease-in-out;
+        transition-delay: 0.2s;
         --map-menu-item-height: 16px;
       }
-
       :host([active]) {
         background: var(--map-menu-active-color);
+        @apply --map-menu-item-active-item;
       }
-
+      paper-button {
+        width: 100%;
+        justify-content: left;
+        margin: 0;
+      }
       iron-icon {
         display: inline-block;
         --iron-icon-height: var(--map-menu-item-height);
       }
-
-      #title {
+      .title {
         text-transform: none;
       }
-
       a,
       a:hover,
       a:visited,
       a:focus {
         color: inherit;
       }
+      #track {
+        transition: 0.2s all ease-in-out;
+        transition-delay: 0.5s;
+        position: absolute;
+        right: 0;
+
+        margin-right: 0px;
+        width: 0px;
+        height: 0px;
+        visibility: hidden;
+        opacity: 0;
+      }
+      #track.show-icon {
+        margin-right: 5px;
+        width: 18px;
+        height: 18px;
+        visibility: visible;
+        opacity: 1;
+      }
     </style>
-    <a href$="[[url]]">
+    <a tabindex="-1" href$="[[url]]">
       <paper-button id="wrapper" role="link" noink>
-        <template is="dom-if" if="[[__hasIcon(icon)]]">
-          <iron-icon icon="[[icon]]"></iron-icon>
-        </template>
-        <span id="title">[[title]]</span>
+        <iron-icon hidden$="[[__hasIcon(icon)]]" icon="[[icon]]"></iron-icon>
+        <span class="title">[[title]]</span>
+        <iron-icon
+          id="track"
+          hidden$="[[__hasIcon(trackIcon)]]"
+          icon="[[trackIcon]]"
+        ></iron-icon>
       </paper-button>
     </a>
   `,
@@ -46,6 +72,11 @@ Polymer({
     icon: {
       type: String,
       value: ""
+    },
+    trackIcon: {
+      stype: String,
+      value: "",
+      observer: "_trackIconChanged"
     },
     title: {
       type: String,
@@ -72,9 +103,13 @@ Polymer({
   },
 
   observers: ["__selectedChanged(selected, id)"],
-
-  ready: function() {},
-
+  _trackIconChanged: function(newValue, oldValue) {
+    if (newValue) {
+      this.$.track.classList.add("show-icon");
+    } else {
+      this.$.track.classList.remove("show-icon");
+    }
+  },
   __selectedChanged: function(selected, id) {
     if (selected === id) {
       this.fire("active-item", this);
