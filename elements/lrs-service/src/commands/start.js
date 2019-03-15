701b3ac386bb526ff659cf8f3abde6c093cdd331
heyMP <mgp140@gmail.com>
2019-03-15 12:51:21 -0400

    added more cli commands

--- elements/lrs-service/src/commands/start.js
+++ elements/lrs-service/src/commands/start.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { Command, flags } = require("@oclif/command");
 const { spawn } = require("child_process");
 const startServicePath = require.resolve("../../gateway");
@@ -5,17 +6,21 @@ const startServicePath = require.resolve("../../gateway");
 class StartCommand extends Command {
   async run() {
     const { flags } = this.parse(StartCommand);
-    spawn("node", [startServicePath], {
+    const prismaDockerStart = spawn("docker-compose", ["up", "-d"], {
+      cwd: path.join(__dirname, "../../prisma"),
       stdio: "inherit",
       shell: true
+    });
+    prismaDockerStart.on("exit", code => {
+      spawn("node", [startServicePath], {
+        stdio: "inherit",
+        shell: true
+      });
     });
   }
 }
 
-StartCommand.description = `Describe the command here
-...
-Extra documentation goes here
-`;
+StartCommand.description = `Start Prisma Server and Graphql Gateway`;
 
 StartCommand.flags = {
   name: flags.string({ char: "n", description: "name to print" })
