7064a9395bc1f84b3e036a5f7cad3998a07d1819
btopro <btopro@outlook.com>
2019-03-31 16:46:51 -0400

    https://github.com/elmsln/HAXcms/issues/198

---
+++ elements/haxcms-elements/lib/ui-components/magic/active-when-visible.js
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2019 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { store } from "@lrnwebcomponents/haxcms-elements/lib/core/haxcms-site-store.js";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
+import { autorun, toJS } from "mobx";
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/paper-tooltip/paper-tooltip.js";
+/**
+ * `active-when-visible`
+ * `Title of the site`
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class ActiveWhenVisible extends PolymerElement {
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "active-when-visible";
+  }
+  // render function
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        a {
+          height: 10px;
+          width: 10px;
+          float: left;
+          pointer-events: none;
+          background-color: transparent;
+        }
+      </style>
+      <div>
+        <a id="a" href$="[[_a]]" name$="#[[itemId]]" aria-hidden></a>
+        <slot></slot>
+      </div>
+    `;
+  }
+  /**
+   * Props
+   */
+  static get properties() {
+    return {
+      itemId: {
+        type: String
+      },
+      _a: {
+        type: String
+      },
+      thresholds: {
+        type: Array,
+        value: [0.0, 0.25, 0.5, 0.75, 1.0]
+      },
+      rootMargin: {
+        type: String,
+        value: "0px"
+      },
+      visibleLimit: {
+        type: Number,
+        value: 0.5,
+        reflectToAttribute: true
+      },
+      isVisible: {
+        type: Boolean,
+        value: false,
+        notify: true
+      }
+    };
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    // setup the intersection observer
+    this.observer = new IntersectionObserver(
+      this.handleIntersectionCallback.bind(this),
+      {
+        root: document.rootElement,
+        rootMargin: this.rootMargin,
+        threshold: this.thresholds
+      }
+    );
+    this.observer.observe(this);
+  }
+  /**
+   * Handle this being visible
+   */
+  handleIntersectionCallback(entries) {
+    for (let entry of entries) {
+      console.log(entry);
+      if (
+        entry.target === this &&
+        Number(entry.intersectionRatio).toFixed(2) >= this.visibleLimit
+      ) {
+        console.log(this);
+        // now we care
+        if (this.itemId) {
+          let item = store.findItem(this.itemId);
+          this._a = item.location
+            .replace("pages/", "")
+            .replace("/index.html", "");
+          this.isVisible = true;
+          setTimeout(() => {
+            this.$.a.click();
+          }, 50);
+        }
+      }
+    }
+  }
+}
+window.customElements.define(ActiveWhenVisible.tag, ActiveWhenVisible);
+export { ActiveWhenVisible };
