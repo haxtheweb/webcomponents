c69de352f66385cf639f790509521d8ca6877476
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2019-06-11 16:43:59 -0400

    hax-text-editor in progress has new buttons and has fixed pop-ups, but it needs to be able to do something with those buttons

---
+++ elements/hax-text-editor/src/hax-text-editor.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2019 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { RichTextEditor } from "@lrnwebcomponents/rich-text-editor/rich-text-editor.js";
+import "./lib/hax-text-editor-toolbar.js";
+
+/**
+ * `hax-text-editor`
+ * `rich-text-editor configured for HAX`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class HaxTextEditor extends RichTextEditor {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+  constructor() {
+    super();
+    this.type = "hax-text-editor-toolbar";
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "hax-text-editor";
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    console.log("HAX connectedCallback", document, document.head);
+  }
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  //disconnectedCallback() {}
+}
+window.customElements.define(HaxTextEditor.tag, HaxTextEditor);
+export { HaxTextEditor };
