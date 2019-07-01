660c5a8f954375899836972b95773b8bb40a8f6f
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2019-07-01 08:47:41 -0400

    hax-text-editor-inline button

---
+++ elements/hax-text-editor/lib/hax-text-editor-inline.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2019 Penn State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { RichTextEditorPromptButton } from "@lrnwebcomponents/rich-text-editor/lib/buttons/rich-text-editor-prompt-button.js";
+/**
+ * `hax-text-editor-inline`
+ * `a button for inline HAX widgets in hax-text-editor (custom buttons can extend this)`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @polymer
+ */
+class HaxTextEditorInline extends RichTextEditorPromptButton {
+  constructor() {
+    super();
+    this.inlineWidget = true;
+    this.toggles = true;
+    this.value = {
+      "": null
+    };
+  }
+
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {
+      element: {
+        type: Object,
+        value: null
+      },
+      tag: {
+        type: String,
+        computed: "_getTag(element)"
+      },
+      icon: {
+        type: String,
+        computed: "_getIcon(element)"
+      },
+      label: {
+        type: String,
+        computed: "_getLabel(element)"
+      },
+      fields: {
+        type: String,
+        computed: "_getFields(element)"
+      }
+    };
+  }
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  static get tag() {
+    return "hax-text-editor-inline";
+  }
+  _getTag(element) {
+    return element && element.tag ? element.tag : null;
+  }
+  _getIcon(element) {
+    return element && element.haxProperties && element.haxProperties.gizmo
+      ? element.haxProperties.gizmo.icon
+      : null;
+  }
+  _getLabel(element) {
+    return element && element.haxProperties && element.haxProperties.gizmo
+      ? element.haxProperties.gizmo.title
+      : null;
+  }
+  _getFields(element) {
+    return element && element.haxProperties && element.haxProperties.settings
+      ? element.haxProperties.settings.configure
+      : null;
+  }
+}
+window.customElements.define(HaxTextEditorInline.tag, HaxTextEditorInline);
+export { HaxTextEditorInline };
