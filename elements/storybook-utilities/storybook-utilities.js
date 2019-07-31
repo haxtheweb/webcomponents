6cb85cfe47ae5c2e794e17e74b2419af9027bb0c
btopro <btopro@outlook.com>
2019-07-31 13:29:40 -0400

    silly util thing

--- elements/storybook-utilities/storybook-utilities.js
+++ elements/storybook-utilities/storybook-utilities.js
@@ -1,6 +1,6 @@
 import { storiesOf } from "@storybook/polymer";
 import * as storybookBridge from "@storybook/addon-knobs/polymer";
-import "@lrnwebcomponents/simple-colors/lib/simple-colors.js";
+import "@lrnwebcomponents/simple-colors/simple-colors.js";
 import "@lrnwebcomponents/es-global-bridge/es-global-bridge.js";
 
 /**
