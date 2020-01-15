6e4f14a52aa874e3c6b81e57fc10424648b46254
btopro <btopro@outlook.com>
2020-01-15 10:09:08 -0500

    install for windows

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # where am i? move to where I am. This ensures source is properly sourced
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 # go back a level so we can snag everything
-cd ../elements/
+cd elements
 # walk each directory and update it's demo automatically
 for project in */ ; do
   cd ${project}
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# where am i? move to where I am. This ensures source is properly sourced
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${DIR/$'\r'/} 
 # go back a level so we can snag everything
-cd ../elements/
+cd elements
 # walk each directory and update it's demo automatically
 for project in */ ; do
   cd ${project}
