c410f102aaeed10f8fc8c833b34d11f4b5a1ea14
btopro <btopro@outlook.com>
2020-01-15 10:14:18 -0500

    preagain

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # go back a level so we can snag everything
-cd elements
+cd "elements"
 # walk each directory and update it's demo automatically
 for project in */ ; do
-  cd ${project}
+  cd "${project}"
   rm -rf node_modules
-  cd ../
+  cd "../"
 done
