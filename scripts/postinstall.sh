90a7a7bb4e0426dd4d05a6ef48f346ed430632a6
btopro <btopro@outlook.com>
2020-01-22 11:55:48 -0500

    paths

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 # where am i? move to where I am. This ensures source is properly sourced
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
@@ -6,8 +14,7 @@ cd $DIR
 cd ../elements/
 # walk each directory and update it's demo automatically
 for project in */ ; do
-  cd $DIR
-  cd ../elements/${project}
+  cd ${project}
   p="$(basename -- $project)"
   rm -rf node_modules
   symlink-dir ../../node_modules node_modules
@@ -16,20 +23,54 @@ for project in */ ; do
     rm -rf ../../node_modules/@lrnwebcomponents/${p}
   fi
   mkdir ../../node_modules/@lrnwebcomponents/${p}
-  cd ../../node_modules/@lrnwebcomponents/${p}
-  if [ -f "../../../elements/${p}/${p}.js" ]; then
-    symlink-dir ../../../elements/${p}/${p}.js ${p}.js
+  if [ $machine == "Cygwin" ]; then
+    if [ -f "${p}.js" ]; then
+      symlink-dir ../../../elements/${p}/${p}.js ../../node_modules/@lrnwebcomponents/${p}/${p}.js
+    fi
+    if [ -d "lib" ]; then
+      symlink-dir ../../../elements/${p}/lib ../../node_modules/@lrnwebcomponents/${p}/lib
+    fi
+    if [ -d "build" ]; then
+      symlink-dir ../../../elements/${p}/build ../../node_modules/@lrnwebcomponents/${p}/build
+    fi
+    if [ -d "src" ]; then
+      symlink-dir ../../../elements/${p}/src ../../node_modules/@lrnwebcomponents/${p}/src
+    fi
+    if [ -d "dist" ]; then
+      symlink-dir ../../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
+    fi
+  elif [ "${machine}" == "MinGw" ]; then
+    if [ -f "${p}.js" ]; then
+      symlink-dir ../../../elements/${p}/${p}.js ../../node_modules/@lrnwebcomponents/${p}/${p}.js
+    fi
+    if [ -d "lib" ]; then
+      symlink-dir ../../../elements/${p}/lib ../../node_modules/@lrnwebcomponents/${p}/lib
+    fi
+    if [ -d "build" ]; then
+      symlink-dir ../../../elements/${p}/build ../../node_modules/@lrnwebcomponents/${p}/build
+    fi
+    if [ -d "src" ]; then
+      symlink-dir ../../../elements/${p}/src ../../node_modules/@lrnwebcomponents/${p}/src
+    fi
+    if [ -d "dist" ]; then
+      symlink-dir ../../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
+    fi
+  else
+    if [ -f "${p}.js" ]; then
+      symlink-dir ../../elements/${p}/${p}.js ../../node_modules/@lrnwebcomponents/${p}/${p}.js
+    fi
+    if [ -d "lib" ]; then
+      symlink-dir ../../elements/${p}/lib ../../node_modules/@lrnwebcomponents/${p}/lib
+    fi
+    if [ -d "build" ]; then
+      symlink-dir ../../elements/${p}/build ../../node_modules/@lrnwebcomponents/${p}/build
+    fi
+    if [ -d "src" ]; then
+      symlink-dir ../../elements/${p}/src ../../node_modules/@lrnwebcomponents/${p}/src
+    fi
+    if [ -d "dist" ]; then
+      symlink-dir ../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
+    fi
   fi
-  if [ -d "../../../elements/${p}/lib" ]; then
-    symlink-dir ../../../elements/${p}/lib lib
-  fi
-  if [ -d "../../../elements/${p}/build" ]; then
-    symlink-dir ../../../elements/${p}/build build
-  fi
-  if [ -d "../../../elements/${p}/src" ]; then
-    symlink-dir ../../../elements/${p}/src src
-  fi
-  if [ -d "../../../elements/${p}/dist" ]; then
-    symlink-dir ../../../elements/${p}/dist dist
-  fi
+  cd ../
 done
