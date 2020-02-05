475b2feb304375391576eac736a25656440719fb
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2020-02-05 18:39:32 -0500

    updated relative headings to litelement and used progressive enhancement

--- elements/relative-heading/demo/demo.js
+++ elements/relative-heading/demo/demo.js
@@ -1,9 +1,33 @@
 window.onload = function() {
-  let selector = document.getElementById("selector"),
-    changer = document.getElementById("changer"),
-    rh4 = document.getElementById("rh4");
-  changer.onchange = function(e) {
-    rh4.parentId = changer.value;
-  };
-  selector.removeAttribute("hidden");
+  let changer = document.getElementById("changer"),
+    changer2 = document.getElementById("changer2"),
+    changer3 = document.getElementById("changer3"),
+    sapien = document.getElementById("sapien"),
+    volutpat = document.getElementById("volutpat"),
+    remover = document.getElementById("remover"),
+    lorem = document.getElementById("lorem"),
+    praesent = document.getElementById("praesent"),
+    p = lorem.nextElementSibling, 
+    randomHeading = () => {
+      let h = Math.floor(Math.random() * Math.floor(6)),
+        options = ['aliquam','justo','nec','iaculis','viverra','dignissim'],
+        text = options[Math.floor(Math.random() * Math.floor(5))], 
+        tag = h > 0 ? `h${h}` : 'p';
+      return `<${tag}>Praesent ${text}</${tag}>`;
+    };
+
+  changer.onchange = (e) => {console.log('change',sapien,sapien.parent,changer.value); sapien.parent = changer.value; console.log('change 2',sapien,sapien.parent,changer.value);}
+  changer2.onchange = (e) => volutpat.parent = changer2.value;
+  changer3.onclick = (e) => praesent.innerHTML = randomHeading();
+  remover.onclick = (e) => {
+    if(remover.innerHTML === "Remove lorem"){
+      lorem.remove();
+      p.remove();
+      remover.innerHTML = "Add lorem"
+    } else {
+      praesent.parentNode.insertBefore(lorem,praesent);
+      praesent.parentNode.insertBefore(p,praesent);
+      remover.innerHTML = "Remove lorem";
+    }
+  }
 };
--- elements/relative-heading/src/relative-heading.js
+++ elements/relative-heading/src/relative-heading.js
@@ -3,15 +3,19 @@
  * @license Apache-2.0, see License.md for full text.
  */
 import { LitElement, html, css } from "lit-element/lit-element.js";
-import { wipeSlot } from "@lrnwebcomponents/utils/utils.js";
+import { RelativeHeadingLite } from "./lib/relative-heading-lite.js";
+import "@lrnwebcomponents/anchor-behaviors/anchor-behaviors.js";
+
 /**
  * `relative-heading`
  * `outputs the correct heading hierarchy based on parent heading`
  *
  * @demo demo/index.html
+ * @demo demo/nolinks.html Disable Links
+ * @demo demo/rightalign.html Right-Align Links
  * @customElement relative-heading
  */
-class RelativeHeading extends LitElement {
+class RelativeHeading extends RelativeHeadingLite {
   /* REQUIRED FOR TOOLING DO NOT TOUCH */
 
   /**
@@ -21,58 +25,52 @@ class RelativeHeading extends LitElement {
   static get tag() {
     return "relative-heading";
   }
+
   /**
-   * update this level when the parent id changes
+   * Makes sure there is a utility ready and listening for elements.
    */
-  _getLevel(parentId, defaultLevel) {
-    let parent = document.querySelector("#" + parentId),
-      parentLvl =
-        parent !== null && parent.level !== undefined
-          ? parent.level
-          : defaultLevel - 1,
-      level = parentLvl < 6 ? parentLvl + 1 : 6;
-    return level;
-  }
-  _updateChildren() {
-    document
-      .querySelectorAll('relative-heading[parent-id="' + this.id + '"]')
-      .forEach(child => {
-        child.parentId = null;
-        child.parentId = this.id;
-      });
-  }
-  /**
-   * determines if the level matches a specific level
-   *
-   * @param {number} the heading level
-   * @param {number} the level it might match
-   * @returns {boolean} whether or not they match
-   */
-  _isLevel(level, testLevel) {
-    return level === testLevel;
-  }
   constructor() {
     super();
-    this.defaultLevel = 1;
-    this.parentId = null;
-    this.text = "";
+    this.linkAlignRight = false;
+    this.disableLink = false;
+    this.linkIcon = 'link';
+    this.linkLabel = "Get link"
+    import("@polymer/iron-icons/iron-icons.js");
+    import("@polymer/paper-icon-button/paper-icon-button.js");
   }
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName == "id") {
-        this._updateChildren(this[propName], oldValue);
-      }
-      if (propName == "level" || propName == "text") {
-        wipeSlot(this);
-        let level = document.createElement(`h${this.level}`);
-        level.innerText = this.text;
-        this.appendChild(level);
-        this._updateChildren(this.level, oldValue);
-      }
-      if (["parentId", "defaultLevel"].includes(propName)) {
-        this.level = this._getLevel(this.parentId, this.defaultLevel);
-      }
-    });
+
+  connectedCallback(){
+    super.connectedCallback();
+    if(!this.disableLink) this.manager.useCopyLink();
+  }
+
+  /**
+   * gets whether heading is currently anchored
+   * @readonly
+   * @returns {boolean}
+   */
+  get anchored() {
+    return window.AnchorBehaviors && window.AnchorBehaviors.getTarget 
+      ? window.AnchorBehaviors.getTarget(this) : false;
+  }
+
+  get button(){
+    console.log(this.disableLink);
+    return this.disableLink ? html`` : html`
+      <paper-icon-button  
+        controls="relative-heading-toast"
+        .aria-describedby="${this.id}"
+        .icon="${this.linkIcon}"
+        .title="${this.linkLabel}"
+        ?hidden="${this.disableLink}"
+        ?disabled="${this.disableLink}"
+        @click="${this._handleCopyClick}">
+      </paper-icon-button>
+    `;
+  }
+  _handleCopyClick(){
+    if(!this.disableLink && this.manager && this.manager.copyLink)
+      this.manager.copyLink(this);
   }
 }
 window.customElements.define(RelativeHeading.tag, RelativeHeading);
