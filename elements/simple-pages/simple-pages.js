289ee289c594ae0b7ce8c75f9f871a298d87ecc1
btopro <btopro@outlook.com>
2020-03-17 20:30:59 -0400

    simple-pages

---
+++ elements/simple-pages/simple-pages.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2020 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, css } from "lit-element/lit-element.js";
+
+/**
+ * `simple-pages`
+ * `toggle between pages`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @demo demo/index.html
+ * @customElement simple-pages
+ */
+class SimplePages extends LitElement {
+  
+  // render function
+  render() {
+    return html`
+<style>:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+
+iron-pages:not(:defined) {
+  display: none;
+}</style>
+<iron-pages selected="${this.selected}" selected-attribute="${this.selectedAttribute}" @selected-changed="${this._selectedChanged}">
+  <slot></slot>
+</iron-pages>`;
+  }
+
+  // properties available to the custom element for data binding
+  static get properties() {
+    let props = {
+  "selected": {
+    "type": Number
+  },
+  "selectedAttribute": {
+    "type": String,
+    "attribute": "selected-attribute"
+  }
+}
+;
+    if (super.properties) {
+      props = Object.assign(props, super.properties);
+    }
+    return props;
+  }
+
+  /**
+   * Convention we use
+   */
+  static get tag() {
+    return "simple-pages";
+  }
+
+  /**
+   * HTMLElement
+   */
+  constructor() {
+    super();
+    this.selected = 0;
+    setTimeout(() => {
+      import('@polymer/iron-pages/iron-pages.js');
+    }, 0);
+  }
+  /**
+   * Selected changed
+   */
+  _selectedChanged(e) {
+    if (this.children && this.children[e.detail.value] && this.children[e.detail.value].tagName && this.children[e.detail.value].getAttribute('data-dimport')) {
+      let el = this.children[e.detail.value];
+      if (!window.customElements.get(el.tagName.toLowerCase())) {
+        const basePath = this.pathFromUrl(
+          decodeURIComponent(import.meta.url)
+        );
+        import(`${basePath}../../${el.getAttribute('data-dimport')}`).then((response) => {
+          setTimeout(() => {
+            window.dispatchEvent(new Event("resize"));
+          }, 0);
+        });
+      }
+    }
+  }
+  // simple path from a url modifier
+  pathFromUrl(url) {
+    return url.substring(0, url.lastIndexOf("/") + 1);
+  }
+  /**
+   * LitElement ready
+   */
+  firstUpdated(changedProperties) {}
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      /* notify example
+      // notify
+      if (propName == 'format') {
+        this.dispatchEvent(
+          new CustomEvent(`${propName}-changed`, {
+            detail: {
+              value: this[propName],
+            }
+          })
+        );
+      }
+      */
+      /* observer example
+      if (propName == 'activeNode') {
+        this._activeNodeChanged(this[propName], oldValue);
+      }
+      */
+      /* computed example
+      if (['id', 'selected'].includes(propName)) {
+        this.__selectedChanged(this.selected, this.id);
+      }
+      */
+    });
+  }
+}
+customElements.define(SimplePages.tag, SimplePages);
+export { SimplePages };
---
+++ elements/simple-pages/src/simple-pages.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2020 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, css } from "lit-element/lit-element.js";
+
+/**
+ * `simple-pages`
+ * `toggle between pages`
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @demo demo/index.html
+ * @customElement simple-pages
+ */
+class SimplePages extends LitElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Convention we use
+   */
+  static get tag() {
+    return "simple-pages";
+  }
+
+  /**
+   * HTMLElement
+   */
+  constructor() {
+    super();
+    this.selected = 0;
+    setTimeout(() => {
+      import('@polymer/iron-pages/iron-pages.js');
+    }, 0);
+  }
+  /**
+   * Selected changed
+   */
+  _selectedChanged(e) {
+    if (this.children && this.children[e.detail.value] && this.children[e.detail.value].tagName && this.children[e.detail.value].getAttribute('data-dimport')) {
+      let el = this.children[e.detail.value];
+      if (!window.customElements.get(el.tagName.toLowerCase())) {
+        const basePath = this.pathFromUrl(
+          decodeURIComponent(import.meta.url)
+        );
+        import(`${basePath}../../${el.getAttribute('data-dimport')}`).then((response) => {
+          setTimeout(() => {
+            window.dispatchEvent(new Event("resize"));
+          }, 0);
+        });
+      }
+    }
+  }
+  // simple path from a url modifier
+  pathFromUrl(url) {
+    return url.substring(0, url.lastIndexOf("/") + 1);
+  }
+  /**
+   * LitElement ready
+   */
+  firstUpdated(changedProperties) {}
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      /* notify example
+      // notify
+      if (propName == 'format') {
+        this.dispatchEvent(
+          new CustomEvent(`${propName}-changed`, {
+            detail: {
+              value: this[propName],
+            }
+          })
+        );
+      }
+      */
+      /* observer example
+      if (propName == 'activeNode') {
+        this._activeNodeChanged(this[propName], oldValue);
+      }
+      */
+      /* computed example
+      if (['id', 'selected'].includes(propName)) {
+        this.__selectedChanged(this.selected, this.id);
+      }
+      */
+    });
+  }
+}
+customElements.define(SimplePages.tag, SimplePages);
+export { SimplePages };
