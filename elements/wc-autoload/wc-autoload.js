8fc7e2324ce48b8e876cb4d01446bfa32fe077ed
btopro <btopro@outlook.com>
2020-04-03 14:14:07 -0400

    start of wc-autoloader

--- elements/wc-autoload/wc-autoload.js
+++ elements/wc-autoload/wc-autoload.js
@@ -2,14 +2,26 @@
  * Copyright 2020 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
-
+import "@lrnwebcomponents/dynamic-import-registry/dynamic-import-registry.js";
+// register globally so we can make sure there is only one
+window.WCAutoload = window.WCAutoload || {};
+// request if this exists. This helps invoke the element existing in the dom
+// as well as that there is only one of them. That way we can ensure everything
+// is rendered through the same modal
+window.WCAutoload.requestAvailability = () => {
+  if (!window.WCAutoload.instance) {
+    window.WCAutoload.instance = document.createElement(
+      "wc-autoload"
+    );
+    document.body.appendChild(window.WCAutoload.instance);
+  }
+  return window.WCAutoload.instance;
+};
+// forces self appending
+window.WCAutoload.requestAvailability();
 /**
  * `wc-autoload`
  * `automatically load new tags in the dom`
- *
- * @microcopy - language worth noting:
- *  -
- *
  * @demo demo/index.html
  * @element wc-autoload
  */
@@ -19,6 +31,40 @@ class WcAutoload extends HTMLElement {
    */
   static get tag() {
     return "wc-autoload";
+  }
+  constructor() {
+    super();
+    this.registry = window.DynamicImportRegistry.requestAvailability();
+    this.list = window.WCAutoload.registry;
+  }
+  connectedCallback() {
+    this._mutationObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          if (node.tagName) {
+            this.processNewElement(node);
+          }
+        });
+      });
+    });
+    this._mutationObserver.observe(document.querySelector("body"), {
+      childList: true,
+      subtree: true
+    });
+  }
+  processElementList(list) {
+    for (var i = 0; i < list.length; i++) {
+      this.processNewElement(list[i]);
+    }
+  }
+  disconnectedCallback() {
+    this._mutationObserver.disconnect();
+  }
+  /**
+   * Process new elements
+   */
+  processNewElement(node) {
+    this.registry.loadDefinition(node.tagName);
   }
 }
 window.customElements.define(WcAutoload.tag, WcAutoload);
