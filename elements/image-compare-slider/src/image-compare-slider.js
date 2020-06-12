88250b3890e4c20c2d6f133627f5099e5b2ae6fd
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2020-06-12 11:58:00 -0400

    added longdesc support for accessibility

--- elements/image-compare-slider/src/image-compare-slider.js
+++ elements/image-compare-slider/src/image-compare-slider.js
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit-element/lit-element.js";
 import { SchemaBehaviors } from "@lrnwebcomponents/schema-behaviors/schema-behaviors.js";
+import { A11yDetails } from "@lrnwebcomponents/a11y-details/a11y-details.js";
 /**
  * `image-compare-slider`
  * Layers images over each other with a slider interface to compare them
@@ -15,24 +16,51 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
       css`
         :host {
           display: inline-flex;
-          width: 100%;
+          margin: 15px 0;
         }
-        :host > div,
-        :host #container,
-        :host #top {
-          width: 100%;
+        :host([hidden]) {
+          display: none !important;
         }
-        :host #container {
-          background-size: cover;
-          overflow: visible;
+        figure {
+          margin: 0;
         }
-        :host #top {
-          background-size: auto 100%;
+        #description > div {
+          display: flex;
+          align-items:flex-end;
+          justify-content: space-between;
+        }
+        a11y-details {
+          font-size: 85%;
+        }
+        #container,
+        #container > div {
+          margin: 0;
+          padding: 0;
+        }
+        #container > div {
+          position: relative;
+        }
+        #top {
+          top: 0;
+          left: 0;
+          position: absolute;
+          opacity: var(--image-compare-slider-opacity, 1);
+          width: var(--image-compare-slider-width, 50%);
           overflow: hidden;
         }
-        :host #slider {
-          width: calc(100% + 30px);
-          margin-left: -15px;
+        #top img {
+          width: var(--image-compare-slider-image-width);
+        }
+        #top img, 
+        #bottom {
+          display: block;
+        }
+        #bottom {
+          width: 100%;
+        }
+        #slider {
+          width: calc(100% + 32px);
+          margin: -15px 0 0 -16px;
         }
       `
     ];
@@ -46,24 +74,47 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
   }
   render() {
     return html`
-      <div>
-        ${this.title
-          ? html`
-              <h2>${this.title}</h2>
-            `
-          : ""}
-        <slot name="heading"></slot>
-        <slot name="description"></slot>
-        <slot name="top" hidden></slot>
-        <slot name="bottom" hidden></slot>
-        <div id="container" style="background-image: url(${this.bottomSrc});">
-          <div id="top" style="background-image: url(${this.topSrc});"></div>
+      <figure>
+        <figcaption>
+          ${this.title
+            ? html`
+                <h2>${this.title}</h2>
+              `
+            : ""}
+          <slot name="heading"></slot>
+          <div id="description">
+            <slot name="description"></slot>
+            <div>
+              <a11y-details id="top-desc" 
+                ?hidden="${!this.topLongdesc}">
+                <details>
+                  <summary>${this.topAlt}</summary>
+                  ${this.topLongdesc}
+                </details>
+              </a11y-details>
+              <a11y-details id="bottom-desc" 
+                ?hidden="${!this.bottomLongdesc}">
+                <details>
+                  <summary>${this.bottomAlt}</summary>
+                  ${this.bottomLongdesc}
+                </details>
+              </a11y-details>
+            </div>
+          </div>
+        </figcaption>
+        <div id="container">
+          <div>
+            <img id="bottom" src="${this.bottomSrc}" alt="${this.bottomAlt}" aria-describedby="${this.bottomDescribedby}"/>
+            <div id="top">
+              <img src="${this.topSrc}" alt="${this.topAlt}" aria-describedby="${this.topDescribedby}"/>
+            </div>
+          </div>
         </div>
         <user-action track="click">
           <paper-slider id="slider" value="50"></paper-slider>
         </user-action>
         <div></div>
-      </div>
+      </figure>
     `;
   }
 
@@ -75,17 +126,32 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
     return {
       ...super.properties,
       /**
-       * @deprecated Use `slot=heading`
+       * src for top image
        */
-      title: {
-        type: String
+      bottomAlt: {
+        type: String,
+        attribute: "bottom-alt"
       },
       /**
        * src for top image
        */
-      topSrc: {
+      bottomAriaDescribedby: {
         type: String,
-        attribute: "top-src"
+        attribute: "bottom-aria-describedby"
+      },
+      /**
+       * src for top image
+       */
+      bottomLongdesc: {
+        type: String,
+        attribute: "bottom-longdesc"
+      },
+      /**
+       * src for top image
+       */
+      bottomSrc: {
+        type: String,
+        attribute: "bottom-src"
       },
       /**
        * mode for the slider: wipe
@@ -94,11 +160,38 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
         type: Boolean
       },
       /**
+       * @deprecated Use `slot=heading`
+       */
+      title: {
+        type: String
+      },
+      /**
        * src for top image
        */
-      bottomSrc: {
+      topAlt: {
         type: String,
-        attribute: "bottom-src"
+        attribute: "top-alt"
+      },
+      /**
+       * src for top image
+       */
+      topAriaDescribedby: {
+        type: String,
+        attribute: "top-aria-describedby"
+      },
+      /**
+       * src for top image
+       */
+      topLongdesc: {
+        type: String,
+        attribute: "top-longdesc"
+      },
+      /**
+       * src for top image
+       */
+      topSrc: {
+        type: String,
+        attribute: "top-src"
       }
     };
   }
@@ -128,7 +221,6 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
       if (["topSrc"].includes(propName)) {
-        this._updateAspect();
         this._slide();
       }
     });
@@ -189,29 +281,75 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
           {
             property: "topSrc",
             title: "Top image",
-            description: "The top image that swipes over",
+            description: "The base image to swipe over",
             inputMethod: "haxupload",
-            validationType: "url"
+            validationType: "url",
+            required: true
+          },
+          {
+            property: "topAlt",
+            title: "Top image alt text",
+            description: "Required alternate text for accessibility",
+            inputMethod: "alt",
+            required: true
+          },
+          {
+            slot: "topDesc",
+            title: "Top image description",
+            description: "Recommended long description for accessibility",
+            inputMethod: "code-editor"
           },
           {
             property: "bottomSrc",
             title: "Bottom image",
             description: "The base image to swipe over",
             inputMethod: "haxupload",
-            validationType: "url"
+            validationType: "url",
+            required: true
+          },
+          {
+            property: "bottomAlt",
+            title: "Bottom image alt text",
+            description: "Required alternate text for accessibility",
+            inputMethod: "alt",
+            required: true
+          },
+          {
+            slot: "bottomDesc",
+            title: "Bottom image description",
+            description: "Recommended long description for accessibility",
+            inputMethod: "code-editor"
           }
         ],
-        advanced: []
+        advanced: [
+          {
+            property: "topAriaDescribedby",
+            title: "Top aria-decsribedby",
+            description: "Space-separated id list for long descriptions that appear elsewhere",
+            inputMethod: "textfield"
+          },
+          {
+            property: "bottomAriaDescribedby",
+            title: "Bottom aria-decsribedby",
+            description: "Space-separated id list for long descriptions that appear elsewhere",
+            inputMethod: "textfield"
+          }
+
+        ]
       },
       demoSchema: [
         {
           tag: "image-compare-slider",
           properties: {
-            topSrc: new URL(`./demo/images/Matterhorn01.png`, import.meta.url),
+            topSrc: new URL(`./demo/images/Matterhorn02.png`, import.meta.url),
+            topAlt: "Matterhorn no snow.",
+            topLongdesc: `<p>The image on the top or when slider is moved all the way to the right is the Matterhorn on a cloudy day without snow.</p>`,
             bottomSrc: new URL(
-              `./demo/images/Matterhorn02.png`,
+              `./demo/images/Matterhorn01.png`,
               import.meta.url
             ),
+            bottomAlt: "Matterhorn with snow.",
+            bottomLongdesc: `<p>As you move the slider to the left, the image below it reveals the Matterhorn on a clear day with snow.</p>`,
             style: "width:100%;max-width:400px"
           },
           content: `<h2 slot="heading">Image Compare Slider Default</h2><p slot="description">The slider will fade away the top image.</p>`
@@ -220,14 +358,18 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
           tag: "image-compare-slider",
           properties: {
             opacity: true,
-            topSrc: new URL(`./demo/images/Matterhorn01.png`, import.meta.url),
+            topSrc: new URL(`./demo/images/Matterhorn02.png`, import.meta.url),
+            topAlt: "Matterhorn no snow.",
+            topLongdesc: "longdesc",
             bottomSrc: new URL(
-              `./demo/images/Matterhorn02.png`,
+              `./demo/images/Matterhorn01.png`,
               import.meta.url
             ),
+            bottomAlt: "Matterhorn with snow.",
+            bottomLongdesc: "longdesc",
             style: "width:100%;max-width:400px"
           },
-          content: `<h2 slot="heading">Image Compare Slider Wipe</h2><p slot="description">The slider will wipe away the top image.</p>`
+          content: `<h2 slot="heading">Image Compare Slider Wipe</h2><div slot="description"><p>The slider will wipe away the top image.</p><p id="longdesc">The image on the top or when slider is moved all the way to the right is the Matterhorn on a cloudy day without snow. As you move the slider to the left, the image below it reveals the Matterhorn on a clear day with snow.</p></div>`
         }
       ]
     };
@@ -240,51 +382,67 @@ class ImageCompareSlider extends SchemaBehaviors(LitElement) {
       });
   }
   /**
+   *  calculated aria-describedby attribute for top image
+   *
+   * @readonly
+   * @memberof ImageCompareSlider
+   */
+  get topDescribedby(){
+    this._getDescribedby(this.topLongdesc ? 'top-desc' : undefined,this.topAriaDescribedby);
+  }
+  /**
+   *  calculated aria-describedby attribute for bottom image
+   *
+   * @readonly
+   * @memberof ImageCompareSlider
+   */
+  get bottomDescribedby(){
+    this._getDescribedby(this.bottomLongdesc ? 'bottom-desc' : undefined,this.bottomAriaDescribedby);
+  }
+  /**
+   * calculate aria-describedby attribute for image
+   * 
+   * @param {string} longdesc a long description
+   * @param {string} ariaDescribedBy manual aria-describedby attribute
+   * @returns {string}
+   * @memberof ImageCompareSlider
+   */
+  _getDescribedby(longdesc,ariaDescribedBy){
+    return !longdesc 
+      && !ariaDescribedBy 
+      ? "description" 
+      : `${longdesc} ${ariaDescribedBy}`.trim()
+  }
+  /**
    * updates element based on slotted data
    * @memberof ImageCompareSlider
    */
   slotUpdate() {
-    console.log(
-      this,
-      this.querySelector("[slot=top]"),
-      this.querySelector("[slot=bottom]")
-    );
-    let top = this.querySelector("[slot=top]"),
-      topSrc = top ? top.getAttribute("src") : false,
-      bottom = this.querySelector("[slot=bottom]"),
-      bottomSrc = bottom ? bottom.getAttribute("src") : false;
-    if (topSrc) this.topSrc = topSrc;
-    if (bottomSrc) this.bottomSrc = bottomSrc;
+    this._imageFromSlot("top");
+    this._imageFromSlot("bottom");
+  }
+  _imageFromSlot(type){
+    let image = this.querySelector(`[slot=${type}]`);
+    if(image){
+      if (image.getAttribute("src")) this[`${type}Src`] = image.getAttribute("src");
+      if (image.getAttribute("alt")) this[`${type}Alt`] = image.getAttribute("alt");
+      if (image.getAttribute("aria-describedby")) this[`${type}DescribedBy`] = image.getAttribute("aria-describedby");
+    }
   }
   /**
    * updates the slider
    */
   _slide() {
+    let top = this.shadowRoot.querySelector("#top"), 
+    slider = this.shadowRoot.querySelector("#slider");
     if (this.opacity === false) {
-      this.shadowRoot.querySelector("#top").style.width =
-        this.shadowRoot.querySelector("#slider").immediateValue + "%";
+      top.style.setProperty('--image-compare-slider-width', slider.immediateValue + "%");
+      top.style.setProperty('--image-compare-slider-image-width', (10000/slider.immediateValue) + "%");
+      top.style.setProperty('--image-compare-slider-opacity', 1);
     } else {
-      this.shadowRoot.querySelector("#top").style.opacity =
-        this.shadowRoot.querySelector("#slider").immediateValue / 100;
-    }
-  }
-  /**
-   * updates the aspect ratio
-   */
-  _updateAspect() {
-    let img = document.createElement("img"),
-      el = this.shadowRoot.querySelector("#top"),
-      getAspect = img => {
-        el.style.paddingTop = (img.height * 100) / img.width + "%";
-      };
-    this.__aspect = "75";
-    img.setAttribute("src", this.topSrc);
-    if (img.height !== undefined && img.height > 0) {
-      getAspect(img);
-    } else {
-      img.addEventListener("load", function() {
-        getAspect(img);
-      });
+      top.style.setProperty('--image-compare-slider-width', "100%");
+      top.style.setProperty('--image-compare-slider-image-width',  "100%");
+      top.style.setProperty('--image-compare-slider-opacity', slider.immediateValue / 100);
     }
   }
 }
