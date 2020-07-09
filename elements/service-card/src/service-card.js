547656358eaaee96293675c0cf04ec5b8ba7549b
McGarvelcuddy <54725578+McGarvelcuddy@users.noreply.github.com>
2020-07-09 14:06:53 -0400

    start of https://github.com/elmsln/issues/issues/32

--- elements/service-card/src/service-card.js
+++ elements/service-card/src/service-card.js
@@ -11,54 +11,71 @@ import { LitElement, html, css } from "lit-element/lit-element.js";
  * @element service-card
  */
 class ServiceCard extends LitElement {
-  /* REQUIRED FOR TOOLING DO NOT TOUCH */
-
-  /**
-   * Convention we use
-   */
   static get tag() {
-    return "service-card";
+    return 'service-card';
   }
 
-  /**
-   * HTMLElement
-   */
+  static get properties() {
+    return {
+      image: { type: String },
+      title: { type: String },
+      info: { type: String },
+      alt: { type: String },
+    };
+  }
+
   constructor() {
     super();
+    this.alt = '';
   }
-  /**
-   * LitElement ready
-   */
-  firstUpdated(changedProperties) {}
-  /**
-   * LitElement life cycle - property changed
-   */
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      /* notify example
-      // notify
-      if (propName == 'format') {
-        this.dispatchEvent(
-          new CustomEvent(`${propName}-changed`, {
-            detail: {
-              value: this[propName],
-            }
-          })
-        );
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-flex;
+        margin: 16px;
       }
-      */
-      /* observer example
-      if (propName == 'activeNode') {
-        this._activeNodeChanged(this[propName], oldValue);
+      .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s all ease-in-out;
+        width: var(--service-card-card-width, 300px);
       }
-      */
-      /* computed example
-      if (['id', 'selected'].includes(propName)) {
-        this.__selectedChanged(this.selected, this.id);
+      .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
       }
-      */
-    });
+      .container {
+        padding: 2px 2px 2px 2px;
+      }
+      .avatar {
+        border-radius: 50%;
+        width: calc(var(--service-card-card-width, 300px) - 10px);
+        padding: 5px;
+      }
+      .title {
+        padding: 4px 4px 4px 4px;
+        text-align: center;
+        font-family: 'Courier New', courier, sans-serif;
+      }
+      .info {
+        padding: 4px 4px 4px 4px;
+        height: 125px;
+        overflow-y: auto;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <div class="card">
+        <div class="container">
+          <img loading="lazy" class="avatar" src="${this.image}" alt="${this.alt}" />
+          <h3 class="title"><b>${this.title}</b></h3>
+          <p class="info"><slot></slot></p>
+        </div>
+      </div>
+    `;
   }
 }
+
 customElements.define(ServiceCard.tag, ServiceCard);
 export { ServiceCard };
