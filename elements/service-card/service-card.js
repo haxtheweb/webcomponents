f974368aa104ded3bd8b1b687c1c18cb9d0b9b57
McGarvelcuddy <54725578+McGarvelcuddy@users.noreply.github.com>
2020-07-09 13:34:26 -0400

    date-card https://github.com/elmsln/issues/issues/199 added

---
+++ elements/service-card/service-card.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2020 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, css } from 'lit-element/lit-element.js';
+
+/**
+ * `service-card`
+ * `simple showcase of services provided`
+ * @demo demo/index.html
+ * @element service-card
+ */
+class ServiceCard extends LitElement {
+  
+  //styles function
+  static get styles() {
+    return  [
+      
+      css`
+:host {
+  display: block;
+}
+
+:host([hidden]) {
+  display: none;
+}
+      `
+    ];
+  }
+
+// render function
+  render() {
+    return html`
+
+<slot></slot>`;
+  }
+
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {...super.properties};
+  }
+
+  /**
+   * Convention we use
+   */
+  static get tag() {
+    return "service-card";
+  }
+
+  /**
+   * HTMLElement
+   */
+  constructor() {
+    super();
+    
+  }
+  /**
+   * LitElement ready
+   */
+  firstUpdated(changedProperties) {
+    
+  }
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      /* notify example
+      // notify
+      if (propName == 'format') {
+        this.dispatchEvent(
+          new CustomEvent(`${propName}-changed`, {
+            detail: {
+              value: this[propName],
+            }
+          })
+        );
+      }
+      */
+      /* observer example
+      if (propName == 'activeNode') {
+        this._activeNodeChanged(this[propName], oldValue);
+      }
+      */
+      /* computed example
+      if (['id', 'selected'].includes(propName)) {
+        this.__selectedChanged(this.selected, this.id);
+      }
+      */
+    });
+  }
+  
+}
+customElements.define(ServiceCard.tag, ServiceCard);
+export { ServiceCard };
