6d4c0351b0fa66ae52ca19b355b54ca3f6a192fc
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2020-08-11 12:32:56 -0400

    temporarily saving stuff

---
+++ elements/fullscreen-behaviors/lib/fullscreen-behaviors-manager.js
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2018 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import "@lrnwebcomponents/es-global-bridge/es-global-bridge.js";
+import { LitElement } from "lit-element";
+/**
+ * `fullscreen-behaviors-manager`
+ *
+ * @demo demo/viewer.html
+ * @element fullscreen-behaviors-manager
+ *
+ */
+class FullscreenBehaviorsManager extends LitElement {
+  static get tag() {
+    return "fullscreen-behaviors-manager";
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {
+      __loaded: { type: Boolean },
+      __callbacks: { type: Array }
+    };
+  }
+
+  constructor() {
+    super();
+    this.callbacks = [];
+    this.__loaded = false;
+    if (typeof screenfull === "object") {
+      this._setLoaded();
+    } else {
+      const basePath = this.pathFromUrl(decodeURIComponent(import.meta.url));
+      const location = `${basePath}screenfull/dist/screenfull.js`;
+      window.ESGlobalBridge.requestAvailability();
+      window.ESGlobalBridge.instance.load("screenfullLib", location);
+      window.addEventListener(
+        "es-bridge-screenfullLib-loaded",
+        this._setLoaded
+      );
+    }
+  }
+
+  /**
+   * whether or not the fullscreen mode is be disabled
+   * @returns {boolean}
+   */
+  get enabled() {
+    let mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+    return typeof screenfull === "object" && !mobile;
+  }
+
+  /**
+   * life cycle, element is removed from the DOM
+   */
+  disconnectedCallback() {
+    window.removeEventListener(
+      "es-bridge-screenfullLib-loaded",
+      this._onLoaded
+    );
+    super.disconnectedCallback();
+  }
+
+  /**
+   * once screenfull is loaded, sets the element's __loaded property to true and runs array callbacks 
+   * @param {event} e screenfull load
+   */
+  _setLoaded() {
+    this.__loaded = true;
+    this.__callbacks.forEach(callback=>callback());
+  }
+  /**
+   * runs callback if ready or adds it to array of callbacks to be executed
+   *
+   * @param {function} callback
+   * @memberof FullscreenBehaviorsManager
+   */
+  onReady(callback){
+    if(!this.__loaded) {
+      this.__callbacks.push(callback);
+    } else {
+      callback();
+    }
+  }
+}
+window.customElements.define(FullscreenBehaviorsManager.tag, FullscreenBehaviorsManager);
+export { FullscreenBehaviorsManager };
+
+// register globally so we can make sure there is only one
+window.FullscreenBehaviorsManager = window.FullscreenBehaviorsManager || {};
+// request if this exists. This helps invoke the element existing in the dom
+// as well as that there is only one of them. That way we can ensure everything
+// is rendered through the same modal
+window.FullscreenBehaviorsManager.requestAvailability = () => {
+  if (!window.FullscreenBehaviorsManager.instance) {
+    window.FullscreenBehaviorsManager.instance = document.createElement(
+      "fullscreen-behaviors-manager"
+    );
+    document.body.appendChild(window.FullscreenBehaviorsManager.instance);
+  }
+  return window.FullscreenBehaviorsManager.instance;
+};
+
