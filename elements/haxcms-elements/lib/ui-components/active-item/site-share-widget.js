4b8a4680c9e758b0465f9118f75daf068c6d6b7f
btopro <btopro@outlook.com>
2020-09-17 21:00:29 -0400

    site share widget

--- elements/haxcms-elements/lib/ui-components/active-item/site-share-widget.js
+++ elements/haxcms-elements/lib/ui-components/active-item/site-share-widget.js
@@ -9,7 +9,7 @@ import "web-social-share/dist/esm/web-social-share.entry.js";
 import {
   a as patchEsm,
   b as bootstrapLazy,
-} from "web-social-share/dist/esm/core-a2e7fe62.js";
+} from "web-social-share/dist/esm/index-bef2383e.js";
 
 const defineCustomElements = (win, options) => {
   return patchEsm().then(() => {
