f8f2ea7d80539d8a386fe1644c9dfce935581b7b
btopro <btopro@outlook.com>
2020-11-06 17:29:16 -0500

    search updated

--- elements/simple-search/simple-search.js
+++ elements/simple-search/simple-search.js
@@ -4,8 +4,8 @@
  */
 import { LitElement, html, css } from "lit-element/lit-element.js";
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
+import "@lrnwebcomponents/simple-fields/lib/simple-fields-field.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
-import "@polymer/paper-input/paper-input.js";
 import "@lrnwebcomponents/simple-tooltip/simple-tooltip.js";
 import "./lib/simple-search-content.js";
 import "./lib/simple-search-match.js";
@@ -56,18 +56,8 @@ class SimpleSearch extends LitElement {
           margin-right: 4px;
           padding: var(--simple-search-padding, unset);
           margin: var(--simple-search-margin, unset);
-          --paper-input-container-input-color: var(
-            --simple-search-input-text-color,
-            #000
-          );
-          --paper-input-container-shared-input-style_-_color: var(
-            --simple-search-input-text-color,
-            #000
-          );
-          --paper-input-container-focus-color: var(
-            --simple-search-input-line-color,
-            #000
-          );
+          --simple-fields-color: var(--simple-search-input-text-color, #000);
+          --simple-fields-color: var(--simple-search-input-line-color, #000);
           --paper-input-container-color: var(
             --simple-search-input-placeholder-color,
             #222
@@ -114,15 +104,15 @@ class SimpleSearch extends LitElement {
 
   // render function
   render() {
-    return html` <paper-input
+    return html` <simple-fields-field
         id="input"
         label="${this.searchInputLabel}"
         ?always-float-label="${this.alwaysFloatLabel}"
         ?no-label-float="${this.noLabelFloat}"
-        @change="${this._handleChange}"
+        @value-changed="${this._handleChange}"
       >
         <simple-icon icon="${this.searchInputIcon}" slot="prefix"></simple-icon>
-      </paper-input>
+      </simple-fields-field>
       <div id="xofy" ?shrink-hide="${this._hasNoSearch(this.searchTerms)}">
         ${this._getResultsSpan(this.resultPointer, this.resultCount)}
       </div>
--- elements/simple-search/src/simple-search.js
+++ elements/simple-search/src/simple-search.js
@@ -4,8 +4,8 @@
  */
 import { LitElement, html, css } from "lit-element/lit-element.js";
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
+import "@lrnwebcomponents/simple-fields/lib/simple-fields-field.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
-import "@polymer/paper-input/paper-input.js";
 import "@lrnwebcomponents/simple-tooltip/simple-tooltip.js";
 import "./lib/simple-search-content.js";
 import "./lib/simple-search-match.js";
