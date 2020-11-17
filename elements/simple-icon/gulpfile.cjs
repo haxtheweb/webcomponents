2dcac47cf1492889731bbf984d25f336ad372f4d
btopro <btopro@outlook.com>
2020-11-17 18:11:32 -0500

    https://github.com/elmsln/issues/issues/430

--- elements/simple-icon/gulpfile.cjs
+++ elements/simple-icon/gulpfile.cjs
@@ -7,6 +7,7 @@ const replace = require("gulp-replace");
 const stripCssComments = require("strip-css-comments");
 const decomment = require("decomment");
 const packageJson = require("./package.json");
+
 // merge all the src files together
 gulp.task("merge", () => {
   return gulp
@@ -142,4 +143,39 @@ gulp.task("watch", () => {
 
 gulp.task("dev", gulp.series("merge", "analyze", "watch"));
 
-gulp.task("default", gulp.series("merge", "analyze", "compile"));
+// walk the tree and build the icon structure
+function dirTree(filename) {
+  var stats = fs.lstatSync(filename),
+      info;
+  // go deeper for the directory as it's got yummy icons
+  if (stats.isDirectory()) {
+    info = {
+      name: path.basename(filename),
+      path: filename,
+      icons: fs.readdirSync(filename).map(function(child) {
+        return dirTree(filename + '/' + child);
+      })
+    };
+  }
+  // sniff for svg's
+  else if (path.basename(filename).includes('.svg')) {
+    info = path.basename(filename).replace('.svg','');
+  }
+  return info;
+}
+
+// discover iconset and build json structure
+gulp.task("iconset", (done) => {
+  const path = "./lib/svgs";
+  const jsonContent = JSON.stringify(dirTree(path).icons, null, 2);  
+  fs.writeFile("./lib/iconsets.json", jsonContent, 'utf8', function (err) {
+      if (err) {
+          console.log("An error occured while writing JSON Object to File.");
+          return console.log(err);
+      }
+      console.log("JSON file has been saved.");
+      return true;
+  });
+  done();
+});
+gulp.task("default", gulp.series("merge", "analyze", "compile", "iconset"));
