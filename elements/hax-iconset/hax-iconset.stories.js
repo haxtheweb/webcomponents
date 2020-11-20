555a739caadf6b82f8e9378e937bd3b785921ee9
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2020-11-20 10:09:32 -0500

    hax-iconset demo for storybook

--- elements/hax-iconset/hax-iconset.stories.js
+++ elements/hax-iconset/hax-iconset.stories.js
@@ -1,56 +1,32 @@
 import { html } from "lit-element/lit-element.js";
-import "@polymer/iron-icon/iron-icon.js";
+import { SimpleIconsetDemo } from "@lrnwebcomponents/simple-icon/lib/simple-iconset-demo.js";
+import { SimpleIconLite } from "@lrnwebcomponents/simple-icon/lib/simple-icon-lite.js";
+import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 import "@lrnwebcomponents/hax-iconset/hax-iconset.js";
+import { HaxIconsetData } from "@lrnwebcomponents/hax-iconset/lib/iconset.js";
 import {
   withKnobs,
   withWebComponentsKnobs,
   select,
+  number,
+  boolean,
   text,
 } from "@open-wc/demoing-storybook";
 import { StorybookUtilities } from "@lrnwebcomponents/storybook-utilities/storybook-utilities.js";
 
 export default {
   title: "Media|Icons",
-  component: "hax-iconset",
+  component: "simple-icons",
   decorators: [withKnobs, withWebComponentsKnobs],
   parameters: {
     options: { selectedPanel: "storybookjs/knobs/panel" },
   },
 };
-const utils = new StorybookUtilities();
+const utils = new StorybookUtilities(),
+  json = new URL("./lib/iconsets.json", import.meta.url),
+  href = json && json.href ? json.href : "";
 export const HaxIconsetStory = () => {
-  return utils.getDemo(
-    `<iconset-demo include-sets="hax"></iconset-demo>`,
-    `<p>The following is a full list of hax-iconset's icons.</p>`
-  );
-};
-
-export const HaxIconsetDemo = () => {
-  let iconset = document.createElement("iconset-demo");
-  iconset.includeSets = "hax";
-  iconset.hidden = "hidden";
-  document.body.appendChild(iconset);
-
-  let icons =
-      iconset && iconset.__iconList
-        ? iconset.__iconList
-        : [{ name: "icons", icons: ["star", "check", "history"] }],
-    iconlist =
-      icons && icons[0] ? icons[0].icons : ["star", "check", "history"];
-  return utils.getDemo(
-    `<iron-icon 
-      icon="${select(
-        "icon",
-        iconlist,
-        utils.getRandomOption(iconlist),
-        "Properties"
-      )}"
-      style="display:inline-block;width:${text(
-        "width",
-        "",
-        "CSS"
-      )};height:${text("height", "", "CSS")};">
-    </iron-icon>`,
-    `<p>The following is a demo of how to use a hax-iconset icon.</p>`
-  );
+  return html`<simple-iconset-demo
+    .imports=${[HaxIconsetData]}
+  ></simple-iconset-demo>`;
 };
