6f434602b6b172cc86909b67ccf9b37a2884fde1
btopro <btopro@outlook.com>
2021-01-12 18:23:26 -0500

    order for tooling

--- elements/lrn-math/src/lrn-math.js
+++ elements/lrn-math/src/lrn-math.js
@@ -102,9 +102,6 @@ class LrnMathController extends HTMLElement {
   }
 }
 
-export { LrnMathController };
-
-window.customElements.define("lrn-math-controller", LrnMathController);
 /*
 Typesets math written in (La)TeX, using [MathJax](http://mathjax.org).
 ##### Example
@@ -291,5 +288,8 @@ class LrnMath extends HTMLElement {
   }
 }
 
-window.customElements.define(LrnMath.tag, LrnMath);
+window.customElements.define("lrn-math", LrnMath);
 export { LrnMath };
+
+window.customElements.define("lrn-math-controller", LrnMathController);
+export { LrnMathController };
