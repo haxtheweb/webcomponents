0ee17d12e20a556fc28753ece4d108b09810b6fd
Jack Davis <jack.davis@buttercups.co.uk>
2021-02-09 13:35:59 +0000

    Switching the icon to a textfield rather than a select box, adding a boolean option for no-colorizing icons and removing all of the previous logic (now we have a symlink into the config folder for custom icons).

--- elements/course-design/lib/activity-box.js
+++ elements/course-design/lib/activity-box.js
@@ -25,18 +25,7 @@ class ActivityBox extends LitElement {
           float: left;
           position: absolute;
           top: 0px;
-        }
-        :host([icon="settings"]) simple-icon {
-          left: var(--activity-box-icon-settings-left, 0px);
-        }
-        :host([icon="bulb"]) simple-icon {
-          left: var(--activity-box-icon-bulb-left, 0px);
-        }
-        :host([icon="pencil"]) simple-icon {
-          left: var(--activity-box-icon-pencil-left, 0px);
-        }
-        :host([icon="sop"]) simple-icon {
-          left: var(--activity-box-icon-sop-left, 0px);
+          left: var(--activity-box-icon-left, 0px);
         }
         .tag {
           padding: var(--activity-box-content-padding, 0px 0px 0px 85px);
@@ -73,7 +62,10 @@ class ActivityBox extends LitElement {
           margin-bottom: 13px !important;
           max-width: 100%;
         }
-        :host([icon="none"]) .pullout {
+        :host([icon="null"]) .pullout,
+        :host([icon="null"]) .tag,
+        :host([icon=""]) .pullout,
+        :host([icon=""]) .tag {
           padding-left: 10px;
         }
       `,
@@ -103,20 +95,19 @@ class ActivityBox extends LitElement {
             property: "icon",
             title: "Icon",
             description: "The icon to be displayed alongside the activity box",
-            inputMethod: "select",
-            options: {
-              none: "None",
-              settings: "Settings",
-              bulb: "Bulb",
-              pencil: "Pencil",
-              sop: "Standard Operating Procedure",
-            },
+            inputMethod: "textfield",
           },
           {
             property: "tag",
             title: "Activity box tag",
             description: "The tag to be displayed within the activity box",
             inputMethod: "textfield",
+          },
+          {
+            property: "nocolourize",
+            title: "No Colorize",
+            description: "Check to stop any colors being applied to the icon",
+            inputMethod: "boolean",
           },
         ],
         advanced: [],
@@ -145,14 +136,9 @@ class ActivityBox extends LitElement {
         type: String,
         reflect: true,
       },
-      /* The variable used for passing the icon to the simple-icon component */
-      _simpleIcon: {
-        type: String,
-        reflect: false,
-      },
-      /* The variable used for passing a source href to the simple-icon component */
-      _simpleIconSource: {
-        type: String,
+      /* Whether or not the icon should have color applied */
+      nocolourize: {
+        type: Boolean,
         reflect: false,
       },
     };
@@ -160,58 +146,18 @@ class ActivityBox extends LitElement {
 
   constructor() {
     super();
-    this.icon = "bulb";
+    this.icon = "";
     this.tag = "";
-    this._simpleIcon = this.icon;
-    this._simpleIconSource = "";
-  }
-
-  updateIconSource(context, icon, source) {
-    this._simpleIcon = icon;
-    this._simpleIconSource = source;
-  }
-
-  updated(changedProperties) {
-    if (super.updated) {
-      super.updated(changedProperties);
-    }
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName == "icon") {
-        var customIcon = getComputedStyle(this)
-          .getPropertyValue("--activity-box-icon-" + `${this[propName]}`)
-          .slice(5, -1)
-          .replace(/\\/g, "");
-        if (customIcon) {
-          this.updateIconSource(this, "", customIcon);
-        } else {
-          /* For certain icons, we need to switch from the friendly name to the actual name of the default icon */
-          var icon = `${this[propName]}`;
-          switch (this[propName]) {
-            case "pencil":
-              icon = "editor:mode-edit";
-              break;
-            case "sop":
-              icon = "editor:format-list-bulleted";
-              break;
-            case "bulb":
-              icon = "lightbulb-outline";
-              break;
-          }
-          this.updateIconSource(this, icon, "");
-        }
-      }
-    });
   }
 
   render() {
     return html`
       <div class="container">
-        ${this._simpleIcon !== "none"
+        ${this.icon
           ? html`
               <simple-icon
-                icon="${this._simpleIcon}"
-                src="${this._simpleIconSource}"
-                ?no-colorize=${this._simpleIconSource}
+                icon="${this.icon}"
+                ?no-colorize=${this.nocolourize}
               ></simple-icon>
             `
           : html``}
