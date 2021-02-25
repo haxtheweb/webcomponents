149a422c48d1bfd55a263bf2406e3354b2706366
btopro <btopro@outlook.com>
2021-02-25 16:34:42 -0500

    i18n-manager start

---
+++ elements/i18n-manager/i18n-manager.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2021 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, css } from "lit-element/lit-element.js";
+/**
+ * `i18n-manager`
+ * `internationalization management singleton and helper classes`
+ * @demo demo/index.html
+ * @element i18n-manager
+ */
+class I18NManager extends LitElement {
+  /**
+   * HTMLElement
+   */
+  constructor() {
+    super();
+  }
+  /**
+   * LitElement style callback
+   */
+  static get styles() {
+    // support for using in other classes
+    let styles = [];
+    if (super.styles) {
+      styles = super.styles;
+    }
+    return [
+      ...styles,
+      css`
+        :host {
+          display: block;
+        }
+      `,
+    ];
+  }
+  /**
+   * LitElement render callback
+   */
+  render() {
+    return html`
+      <div>
+        <slot></slot>
+      </div>
+    `;
+  }
+  /**
+   * Convention we use
+   */
+  static get tag() {
+    return "i18n-manager";
+  }
+  /**
+   * LitElement ready
+   */
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+  }
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    changedProperties.forEach((oldValue, propName) => {
+      /* notify example
+      // notify
+      if (propName == 'format') {
+        this.dispatchEvent(
+          new CustomEvent(`${propName}-changed`, {
+            detail: {
+              value: this[propName],
+            }
+          })
+        );
+      }
+      */
+      /* observer example
+      if (propName == 'activeNode') {
+        this._activeNodeChanged(this[propName], oldValue);
+      }
+      */
+      /* computed example
+      if (['id', 'selected'].includes(propName)) {
+        this.__selectedChanged(this.selected, this.id);
+      }
+      */
+    });
+  }
+}
+customElements.define(I18NManager.tag, I18NManager);
+export { I18NManager };
