df12b2d7c0f79115ba749fe5d355768da855acab
Collin Kleest <collinkleest@gmail.com>
2021-03-03 13:39:09 -0500

    add bootstrap dependencies

--- elements/bootstrap-theme/src/bootstrap-theme.js
+++ elements/bootstrap-theme/src/bootstrap-theme.js
@@ -4,6 +4,8 @@
  */
 import { LitElement, html, css } from "lit-element/lit-element.js";
 import { HAXCMSLitElementTheme } from "@lrnwebcomponents/haxcms-elements/lib/core/HAXCMSLitElementTheme.js";
+import "jquery/dist/jquery.js";
+import "bootstrap/dist/js/bootstrap.js";
 
 /**
  * `bootstrap-theme`
@@ -11,7 +13,7 @@ import { HAXCMSLitElementTheme } from "@lrnwebcomponents/haxcms-elements/lib/cor
  * @demo demo/index.html
  * @element bootstrap-theme
  */
-class BootstrapTheme extends HAXCMSLitElementTheme(LitElement) {
+class BootstrapTheme extends HAXCMSLitElementTheme {
   /**
    * Convention we use
    */
@@ -39,27 +41,39 @@ class BootstrapTheme extends HAXCMSLitElementTheme(LitElement) {
 
   render() {
     return html`
-      <nav id="bootstrap-menu" role="navigation" aria-labelledby="sitemenu">
-        <replace-tag
-          with="site-menu"
-          id="sitemenu"
-          import-method="view"
-        ></replace-tag>
-      </nav>
-      <main>
-        <article>
-          <section>
-            <slot></slot>
-          </section>
-        </article>
-      </main>
+      <link
+        rel="stylesheet"
+        href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+      />
+      <div class="site">
+        <div class="menuoutline">
+          <nav id="bootstrap-menu" role="navigation" aria-labelledby="sitemenu">
+            <replace-tag
+              with="site-menu"
+              id="sitemenu"
+              import-method="view"
+            ></replace-tag>
+          </nav>
+        </div>
+        <main>
+          <article id="contentcontainer">
+            <section id="slot">
+              <slot></slot>
+            </section>
+          </article>
+        </main>
+      </div>
     `;
   }
 
   /**
    * LitElement ready
    */
-  firstUpdated(changedProperties) {}
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+  }
 
   /**
    * LitElement life cycle - property changed
