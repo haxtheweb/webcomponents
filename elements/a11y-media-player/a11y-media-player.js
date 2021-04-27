8db0ca2fe2bec7c23e7056e21158aa9b7380801c
Griffin Sullivan <48397354+Griffin-Sullivan@users.noreply.github.com>
2021-04-26 22:12:25 -0400

    Delete unused function
    For some reason my latest push didn't actually remove the function but it did locally and says I'm up to date? no idea what happened, but let's do it manually with the GUI!

--- elements/a11y-media-player/a11y-media-player.js
+++ elements/a11y-media-player/a11y-media-player.js
@@ -2896,11 +2896,6 @@ class A11yMediaPlayer extends FullscreenBehaviors(SimpleColors) {
     }
   }
 
-  enableLearningMode() {
-    this.learningMode = true;
-    this.disableSeek = true;
-  }
-
   /**
    * selects `captionsTrack` by key and adjusts `cc` accordingly
    */
