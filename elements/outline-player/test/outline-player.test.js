d1b9bce874c542396ce7fd43297127be81d74cb5
btopro <btopro@outlook.com>
2021-05-11 17:04:39 -0400

    mobx test fixture fixes

--- elements/outline-player/test/outline-player.test.js
+++ elements/outline-player/test/outline-player.test.js
@@ -1,3 +1,9 @@
+// local development and mobx
+window.process = window.process || {
+  env: {
+    NODE_ENV: "development",
+  },
+};
 import {
   expect,
   fixture,
@@ -7,7 +13,7 @@ import {
   fixtureCleanup,
 } from "@open-wc/testing";
 import { setViewport } from "@web/test-runner-commands";
-import "../src/outline-player.js";
+await import("../src/outline-player.js");
 
 /*
  * Instantiation test
