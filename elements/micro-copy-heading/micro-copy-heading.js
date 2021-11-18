ac557de77823776d2d13047a8a74de5d992192b6
btopro <btopro@outlook.com>
2021-11-18 08:58:23 -0500

    deprecated other packages

--- elements/micro-copy-heading/micro-copy-heading.js
+++ elements/micro-copy-heading/micro-copy-heading.js
@@ -2,7 +2,7 @@
  * Copyright 2019 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
-import { LitElement, html, css, customElement, property } from "lit";
+import { LitElement, html, css } from "lit";
 /**
  * `micro-copy-heading`
  * @element micro-copy-heading
--- elements/wave-player/lib/wavesurferjs/src/util/prevent-click.js
+++
@@ -1,8 +0,0 @@
-function preventClickHandler(e) {
-  e.stopPropagation();
-  document.body.removeEventListener("click", preventClickHandler, true);
-}
-
-export default function preventClick(values) {
-  document.body.addEventListener("click", preventClickHandler, true);
-}
