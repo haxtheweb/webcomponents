46ef837729813d0e837f4b3ba577b80f6aa921ae
btopro <btopro@outlook.com>
2021-12-07 15:51:19 -0500

    supported added for flag icons

--- elements/simple-icon/lib/simple-icons.js
+++ elements/simple-icon/lib/simple-icons.js
@@ -20,3 +20,21 @@ import { SimpleIconIconsetsManifest } from "./simple-iconset-manifest.js";
     `${new URL("./", import.meta.url).href}svgs/${i}/`
   );
 });
+// flags too but they come from elsewhere
+// ISO 3166-1-alpha-2 Flags
+// via https://flagicons.lipis.dev/
+SimpleIconsetStore.registerIconset(
+  "flags",
+  `${
+    new URL("../../../node_modules/flag-icon-css/flags/4x3", import.meta.url)
+      .href
+  }/`
+);
+// square flag less common but needed ratio
+SimpleIconsetStore.registerIconset(
+  "flags1x1",
+  `${
+    new URL("../../../node_modules/flag-icon-css/flags/1x1", import.meta.url)
+      .href
+  }/`
+);
