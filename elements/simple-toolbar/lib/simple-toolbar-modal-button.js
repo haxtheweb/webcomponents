c9ba3ba65ed07a68b0157edf6fc9c69aa9f042ec
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2022-03-08 14:46:05 -0500

    updates modal button to retain styling

--- elements/simple-toolbar/lib/simple-toolbar-modal-button.js
+++ elements/simple-toolbar/lib/simple-toolbar-modal-button.js
@@ -4,7 +4,8 @@
  */
 import { LitElement, html, css } from "lit";
 import { SimpleToolbarButtonBehaviors } from "./simple-toolbar-button.js";
-import "@lrnwebcomponents/simple-modal/lib/simple-modal-template.js";
+import { SimpleModalStore } from "@lrnwebcomponents/simple-modal/simple-modal.js";
+
 const SimpleToolbarModalButtonBehaviors = function (SuperClass) {
   return class extends SimpleToolbarButtonBehaviors(SuperClass) {
     /**
@@ -17,7 +18,8 @@ const SimpleToolbarModalButtonBehaviors = function (SuperClass) {
       return [
         ...super.styles,
         css`
-          :host([hidden]) {
+          :host([hidden]), 
+          [hidden] {
             display: none !important;
           }
         `,
@@ -31,12 +33,18 @@ const SimpleToolbarModalButtonBehaviors = function (SuperClass) {
           attribute: "id",
           type: String,
           reflect: true
+        },
+        title: {
+          attribute: "string",
+          type: String,
+          reflect: true
         }
       };
     }
 
     constructor() {
       super();
+      window.SimpleModal.requestAvailability();
       window.removeEventListener("simple-modal-hide", this._handleModalClose.bind(this));
       window.removeEventListener("simple-modal-show", this._handleModalOpen.bind(this));
     }
@@ -44,73 +52,64 @@ const SimpleToolbarModalButtonBehaviors = function (SuperClass) {
     render(){
       return html`${super.render()}${this.modalTemplate}`
     }
-    get modalStyles(){
-      return css`
-        simple-modal-template {
-          --simple-modal-titlebar-color: var(--simple-toolbar-button-bg);
-          --simple-modal-titlebar-background: var(--simple-toolbar-button-color);
-          --simple-modal-header-color: var(--simple-toolbar-button-hover-color);
-          --simple-modal-header-background: var(--simple-toolbar-button-hover-bg);
-          --simple-modal-content-container-color: var(--simple-toolbar-button-color);
-          --simple-modal-content-container-background: var(--simple-toolbar-button-bg);
-          --simple-modal-buttons-color: var(--simple-toolbar-button-color);
-          --simple-modal-buttons-background: var(--simple-toolbar-button-bg);
-        }
-      `;
-    }
 
     get modalTemplate(){
       return html`
-        <simple-modal-template modal-id="${this.id || 'modal-button'}-modal" title="${this.label}">
-          ${this.headerTemplate}
-          ${this.precontentTemplate}
-          ${this.contentTemplate}
-          ${this.customTemplate}
-          ${this.buttonsTemplate}
-        </simple-modal-template>
-        <slot></slot>
+        <slot hidden name="header"></slot>
+        <slot hidden name="precontent"></slot>
+        <slot hidden name="content"></slot>
+        <slot hidden name="buttons"></slot>
+        <slot hidden name="custom"></slot>
       `;
-    }
-
-    get headerTemplate(){
-      return;
-    }
-
-    get contentTemplate(){
-      return;
-    }
-
-    get precontentTemplate(){
-      return;
-    }
-
-    get buttonsTemplate(){
-      return;
-    }
-
-    get customTemplate(){
-      return;
-    }
-
-    get shadowModal(){
-      return this.shadowRoot 
-        && this.shadowRoot.querySelector('simple-modal-template') 
-        ? this.shadowRoot.querySelector('simple-modal-template') 
-        : undefined;
     }
 
     _handleModalClose(e){
       this.toggled = false;
-      this.__children.forEach(child=>this.append(child));
-      this.__children = undefined;
     }
 
     _handleModalOpen(e){
       if(e.detail && e.deal.invokedBy && e.deal.invokedBy == this) this.toggled = true;
     }
 
+    _getModalStyles(target){
+      let styles = getComputedStyle(target);
+      return {
+        "--simple-modal-titlebar-color": styles.getPropertyValue('--simple-toolbar-button-bg'),
+        "--simple-modal-titlebar-background": styles.getPropertyValue('--simple-toolbar-button-color'),
+        "--simple-modal-header-color": styles.getPropertyValue('--simple-toolbar-button-hover-color'),
+        "--simple-modal-header-background": styles.getPropertyValue('--simple-toolbar-button-hover-bg'),
+        "--simple-modal-content-container-color": styles.getPropertyValue('--simple-toolbar-button-color'),
+        "--simple-modal-content-container-background": styles.getPropertyValue('--simple-toolbar-button-bg'),
+        "--simple-modal-buttons-color": styles.getPropertyValue('--simple-toolbar-button-color'),
+        "--simple-modal-buttons-background": styles.getPropertyValue('--simple-toolbar-button-bg'),
+        "--simple-modal-z-index": Number(styles.getPropertyValue('--simple-toolbar-button-z-index)')+2),
+      };
+    
+    }
+
     _handleClick(e){
       this.toggleModal();
+    }
+
+    /**
+     * toggles button if shortcutKey is pressed
+     *
+     * @param {string} key
+     * @event toggle
+     */
+    _handleShortcutKeys(e, key) {
+      if(!!this.shortcutKeys && this.shortcutKeys != '')this.dispatchEvent(
+        new CustomEvent("toggle", {
+          bubbles: true,
+          cancelable: true,
+          composed: true,
+          detail: {
+            ...e.detail,
+            button: this,
+            shortcutKey: this,
+          },
+        })
+      );
     }
 
     toggleModal(){
@@ -133,11 +132,31 @@ const SimpleToolbarModalButtonBehaviors = function (SuperClass) {
     }
 
     openModal(){
-      if(this.shadowModal && this.shadowModal.openModal) {
-        this.__children = [...this.children];
-        this.__children.forEach(child=>this.shadowModal.append(child));
-        this.shadowModal.openModal();
-      }
+      let styles = this._getModalStyles(this), 
+        elements = {};
+
+      [...this.children].forEach(child=>{
+        let clone = child.cloneNode(true), 
+          slot = child.slot;
+        clone.hidden = false;
+        delete clone.slot;
+        if(!elements[slot]) elements[slot] = [];
+        elements[slot].push(clone);
+      });
+
+      this.dispatchEvent(
+        new CustomEvent("simple-modal-show", {
+          bubbles: true,
+          cancelable: true,
+          detail: {
+            title: this.title || this.label,
+            id: this.id || this.getAttribute('id'),
+            elements: elements,
+            styles: styles,
+            invokedBy: this,
+          }
+        })
+      );
     }
   };
 };
