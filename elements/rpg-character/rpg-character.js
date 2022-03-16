1a10bd56c7c5ff9a19660b42b120644c51d0d41d
btopro <btopro@outlook.com>
2022-03-16 16:01:58 -0400

    svgs added for our character :)

--- elements/rpg-character/rpg-character.js
+++ elements/rpg-character/rpg-character.js
@@ -2,7 +2,7 @@
  * Copyright 2022 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
-import { LitElement, html, css, unsafeCSS } from "lit";
+import { html, css } from "lit";
 import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
 /**
  * `rpg-character`
@@ -12,19 +12,30 @@ import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
  */
 class RpgCharacter extends SimpleColors {
   /**
+   * Convention we use
+   */
+   static get tag() {
+    return "rpg-character";
+  }
+  /**
    * HTMLElement
    */
   constructor() {
     super();
-    this.hatColor = this.randomColor();
-    this.shirtColor = this.randomColor();
-    this.faceColor = this.randomColor();
-    this.pantsColor = this.randomColor();
+    this.base = 0;
+    this.face = 0;
+    this.faceItem = 0;
+    this.skin = 0;
+    this.hair = 0;
     this.accentColor = "orange";
+    this.seed = null;
   }
 
-  randomColor() {
-    return Object.keys(this.colors)[Math.floor(Math.random()*Object.keys(this.colors).length)];
+  randomColor(seed = null) {
+    if (seed === null) {
+      seed = "" + Math.floor(Math.random()*Object.keys(this.colors).length);
+    }
+    return Object.keys(this.colors)[seed];
   }
 
   static get properties() {
@@ -32,9 +43,13 @@ class RpgCharacter extends SimpleColors {
       ...super.properties,
       hatColor: { type: String, attribute: "hat-color" },
       shirtColor: { type: String, attribute: "shirt-color" },
-      faceColor: { type: String, attribute: "face-color" },
       pantsColor: { type: String, attribute: "pants-color" },
-      
+      base: { type: Number },
+      face: { type: Number },
+      faceItem: { type: Number },
+      skin: { type: Number },
+      hair: { type: Number },
+      seed: { type: String },
     }
   }
   /**
@@ -59,6 +74,7 @@ class RpgCharacter extends SimpleColors {
         }
         div {
           width: 113px;
+          transition: .3s ease-in-out background-color;
         }
         #hat {
           height: 55px;
@@ -75,39 +91,63 @@ class RpgCharacter extends SimpleColors {
         `
     ];
   }
+  firstUpdated() {
+    if (this.seed === null) {
+      this.seed = Math.random().toString(36).substring(2,12);
+    }
+  }
   /**
    * LitElement render callback
    */
   render() {
+    const skin = new URL(`./lib/skin/${this.skin}.svg`, import.meta.url).href;
+    const base = new URL(`./lib/base/${this.base}.svg`, import.meta.url).href;
+    const hair = new URL(`./lib/hair/${this.hair}.svg`, import.meta.url).href;
+    const face = new URL(`./lib/face/${this.face}.svg`, import.meta.url).href;
+    const faceItem = new URL(`./lib/faceItem/${this.faceItem}.svg`, import.meta.url).href;
     return html`
-    
-      <img src="${new URL('./lib/base.svg', import.meta.url).href}" />
+      <img src="${skin}" />
+      <img src="${base}" />
+      ${this.base === 1 ? html`<img src="${hair}" />` : ``}
+      <img src="${face}" />
+      <img src="${faceItem}" />
       <div id="hat"></div>
       <div id="face"></div>
       <div id="shirt"></div>
       <div id="pants"></div>
       <style>
         #hat {
-          background-color: var(--simple-colors-default-theme-${this.hatColor}-6, orange);
-        }
-        #face {
-          background-color: var(--simple-colors-default-theme-${this.faceColor}-3, blue);
+          background-color: var(--simple-colors-default-theme-${this.hatColor}-7, orange);
         }
         #shirt {
-          background-color: var(--simple-colors-default-theme-${this.shirtColor}-8, purple);
+          background-color: var(--simple-colors-default-theme-${this.shirtColor}-5, purple);
         }
         #pants {
-          background-color: var(--simple-colors-default-theme-${this.pantsColor}-10, yellow);
+          background-color: var(--simple-colors-default-theme-${this.pantsColor}-8, yellow);
         }
       </style>
     `;
   }
 
-  /**
-   * Convention we use
-   */
-  static get tag() {
-    return "rpg-character";
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === "seed" && this[propName]) {
+        // use the seed to generate a random number
+        let seed = 540;
+        for (let i=0; i < this.seed.length; i++) {
+          seed *= this.seed.charCodeAt(i);
+        }
+        seed = seed.toString();
+        this.skin = seed[0];
+        this.base = seed[1] > 5 ? 1 : 0;
+        this.hair = seed[2];
+        this.face = seed[3] > 5 ? 5 : seed[3];
+        this.faceItem = seed[4];
+        this.hatColor = this.randomColor();
+        this.shirtColor = this.randomColor();
+        this.pantsColor = this.randomColor();
+      }
+    });
   }
 }
 customElements.define(RpgCharacter.tag, RpgCharacter);
