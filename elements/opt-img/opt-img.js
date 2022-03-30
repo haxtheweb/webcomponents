0f7d227634bc7ffe45ac92a7d306ca5d0d0535a4
btopro <btopro@outlook.com>
2022-03-30 16:12:44 -0400

    more comments

--- elements/opt-img/opt-img.js
+++ elements/opt-img/opt-img.js
@@ -13,24 +13,29 @@ import { IntersectionObserverMixin } from "@lrnwebcomponents/intersection-elemen
 class OptImg extends IntersectionObserverMixin(HTMLElement) {
   constructor() {
     super();
+    // alt as nothing for decorative purposes if not set
     this.alt = '';
     this.src = '';
+    // 10% of image visible === visible
     this.IOVisibleLimit = 0.1;
+    // these help w/ threashold being set
     this.IOThresholds = [0.0, 0.10, 0.25, 0.5, 0.75, 1.0];
     this.template = document.createElement("template");
+    // preconnect the domain early on
     this._preconnect = document.createElement("link");
     this._preconnect.rel = "preconnect";
+    // wipe anything that may be here from before
     this.innerHTML = null;
+    // create a 'loading' container
     this._loading = document.createElement("div");
     this._loading.innerHTML = this.loading;
     this.appendChild(this._loading);
   }
-  /**
-   * This is a convention, not the standard to return HTML of the element
-   */
+  // return a basic img tag w/ values printed in
   get html() {
     return `<img src="${this.src}" height="${this.height}" width="${this.width}" alt="${this.alt}" decoding="async" loading="lazy" />`;
   }
+  // get a basic loading svg that's styled
   get loading() {
     return `
     <style>
@@ -74,7 +79,7 @@ class OptImg extends IntersectionObserverMixin(HTMLElement) {
     </svg>`;
   }
   /**
-   * attributes to notice changes to
+   * only update on the 3 values we care about changing
    */
   static get observedAttributes() {
     return ["src", "alt", "element-visible"];
@@ -85,17 +90,20 @@ class OptImg extends IntersectionObserverMixin(HTMLElement) {
   attributeChangedCallback(attr, oldValue, newValue) {
     if (['src','alt'].includes(attr)) {
       this[attr] = newValue;
+      // force a repaint
       this.render();
     }
     else if (['element-visible'].includes(attr)) {
       this.render();
     }
+    // source being set, let's preconnect the domain prior to usage
     if (attr === 'src') {
       // preconnect domain
       this._preconnect.href = new URL(this.src).origin;
       document.head.appendChild(this._preconnect);
     }
   }
+  // peg attribute to property change internally
   set elementVisible(val) {
     if (val === false) {
       this.removeAttribute("element-visible");
@@ -114,27 +122,35 @@ class OptImg extends IntersectionObserverMixin(HTMLElement) {
     return "opt-img";
   }
   /**
-   * Render / rerender the shadowRoot
+   * Render for guts of the tag
    */
   render() {
+    // only render if we're visible
     if (this.elementVisible) {
-      this.template.innerHTML = this.html;
+      // make a fake image
       let i = new Image();
+      // when we load, we'll have the props about it
       i.onload = () => {
+        // subtle, but these 4 lines help reduce the jarring of painting the image
+        // by setting the height/width quickly on the container + img tag
         this.style.height = i.height + 'px';
         this.style.width = i.width + 'px';
         this.height = i.height + 'px';
         this.width = i.width + 'px';
+        // render the actual image happens in this block
+        this.template.innerHTML = this.html;
         this.appendChild(this.template.content);
+        // delay a cycle, undo the style height/width on container
+        // and remove the loading element. this delay helps reduce layout jar
         setTimeout(() => {
-          setTimeout(() => {
-            this._loading.remove();
-          }, 0);
           this.style.height = '';
           this.style.width = '';
+          this._loading.remove();
         }, 0);
       }
+      // setting the src triggers the image to be requested
       i.src = this.src;
+      // delete the preconnect tag in the head for clean up
       this._preconnect.remove();
     }
   }
