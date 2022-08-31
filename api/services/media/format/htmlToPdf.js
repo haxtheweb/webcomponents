d7abba2c0bb9163357ac74e064cc5d39f526d73b
btopro <btopro@outlook.com>
2022-08-31 11:44:21 -0400

    htmlToPdf v to v test

--- api/services/media/format/htmlToPdf.js
+++ api/services/media/format/htmlToPdf.js
@@ -1,26 +1,5 @@
 import { stdPostBody, stdResponse, invalidRequest } from "../../../../utilities/requestHelpers.js";
-import {create as toPDF } from 'html-pdf';
 
-const generatePDF = (html, base = '/') => {
-  return new Promise (
-      (resolve, reject) => {
-          toPDF(html, {
-              format: 'A4',
-              orientation: 'portrait',
-              border: ".5in",
-              timeout: 30000,
-              base: base,
-          }).toBuffer((error, buffer) => {
-              if(error) { 
-                  reject(error) 
-              }
-              else {
-                  resolve(buffer.toString('base64'))
-              }
-          });
-      }
-  )
-}
 
 export default async function handler(req, res) {
   const body = stdPostBody(req);
@@ -31,8 +10,10 @@ export default async function handler(req, res) {
     res = invalidRequest(res, 'missing `html` param');
   }
   else {
+    // need to outsource this endpoint from vercel 1 to 2 for scale reasons
     const base = body.base || '';
     var html = `<html><body>${body.html}</body></html>`;
-    res = stdResponse(res, await generatePDF(html, base), {cache: 86400 });
+    const pdf = await fetch(`https://pdf-from.elmsln.vercel.app/api/pdfFrom?type=html&url=${html}`);
+    res = stdResponse(res, pdf, {cache: 86400 });
   }
 }
