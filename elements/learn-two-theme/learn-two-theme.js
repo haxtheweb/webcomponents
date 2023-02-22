9e7a2070f06f7b5bc9afab9d4f135ba97330d8f4
btopro <btopro@outlook.com>
2023-02-22 12:37:08 -0500

    https://github.com/elmsln/issues/issues/1247

--- elements/learn-two-theme/learn-two-theme.js
+++ elements/learn-two-theme/learn-two-theme.js
@@ -257,11 +257,12 @@ class LearnTwoTheme extends HAXCMSLitElementTheme {
           color: #ffffff;
           padding: 0;
           height: calc(100vh - 98px);
-          --site-menu-active-color: #ffffff;
-          --site-menu-item-active-item-color: var(
-            --simple-colors-default-theme-light-blue-1,
-            rgba(100, 100, 255, 0.1)
-          );
+          --site-menu-font-size: 15px;
+          --site-menu-color: #ffffff;
+          --site-menu-item-active-item-color: var(--simple-colors-default-theme-light-blue-1, rgba(100,100,255,.1));
+          --map-menu-item-a-active-color: white;
+          --map-menu-item-a-active-background-color: rgba(0,0,250,.1);
+          --site-menu-item-active-item-color: rgba(0,0,250,.1);
           overflow: auto;
         }
 
