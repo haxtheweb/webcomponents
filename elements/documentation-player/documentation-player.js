44101c4158f37c5b787361f446f08e5310cc81ae
btopro <btopro@outlook.com>
2023-09-13 11:40:44 -0400

    start of https://github.com/elmsln/issues/issues/1710

---
+++ elements/documentation-player/documentation-player.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2023
+ * @license , see License.md for full text.
+ */
+import { LitElement, html, css } from "lit";
+/**
+ * `documentation-player`
+ * `playlist like element but for documenting web components in the context of hax`
+ * @demo demo/index.html
+ * @element documentation-player
+ */
+class DocumentationPlayer extends LitElement {
+  /**
+   * HTMLElement
+   */
+  constructor() {
+    super();
+  }
+  /**
+   * LitElement style callback
+   */
+  static get styles() {
+    // support for using in other classes
+    let styles = [];
+    if (super.styles) {
+      styles = super.styles;
+    }
+    return [
+      ...styles,
+      css`
+        :host {
+          display: block;
+        }
+      `,
+    ];
+  }
+  /**
+   * LitElement render callback
+   */
+  render() {
+    return html`
+      <div>
+        <slot></slot>
+      </div>
+    `;
+  }
+  /**
+   * Convention we use
+   */
+  static get tag() {
+    return "documentation-player";
+  }
+  /**
+   * LitElement ready
+   */
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+  }
+  /**
+   * LitElement life cycle - property changed
+   */
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    changedProperties.forEach((oldValue, propName) => {
+      /* notify example
+      // notify
+      if (propName == 'format') {
+        this.dispatchEvent(
+          new CustomEvent(`${propName}-changed`, {
+            detail: {
+              value: this[propName],
+            }
+          })
+        );
+      }
+      */
+      /* observer example
+      if (propName == 'activeNode') {
+        this._activeNodeChanged(this[propName], oldValue);
+      }
+      */
+      /* computed example
+      if (['id', 'selected'].includes(propName)) {
+        this.__selectedChanged(this.selected, this.id);
+      }
+      */
+    });
+  }
+}
+customElements.define(DocumentationPlayer.tag, DocumentationPlayer);
+export { DocumentationPlayer };
---
+++ elements/documentation-player/test/documentation-player.test.js
@@ -0,0 +1,19 @@
+import { fixture, expect, html } from "@open-wc/testing";
+import "../documentation-player.js";
+
+describe("elementName test", () => {
+  let element;
+  beforeEach(async () => {
+    element = await fixture(
+      html`<documentation-player></documentation-player>`
+    );
+  });
+
+  it("basic will it blend", async () => {
+    expect(element).to.exist;
+  });
+
+  it("passes the a11y audit", async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
