fdec1958b0cd1a1f6875cc082f3998c363db2838
btopro <btopro@outlook.com>
2023-09-29 13:45:15 -0400

    uuid direct access

--- api/apps/haxcms/lib/JSONOutlineSchema.js
+++ api/apps/haxcms/lib/JSONOutlineSchema.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid/dist/esm-node/v4.js';
 import fetch from "node-fetch";
 import { JSONOutlineSchemaItem } from './JSONOutlineSchemaItem.js';
 /**
--- api/apps/haxcms/lib/JSONOutlineSchemaItem.js
+++ api/apps/haxcms/lib/JSONOutlineSchemaItem.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid/dist/esm-node/v4.js';
 /**
  * JSONOutlineSchemaItem - a single item without an outline of items.
  */
