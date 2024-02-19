4191147af6d4012fdcd17aec9a69aa6173248e54
btopro <btopro@outlook.com>
2024-02-19 14:34:13 -0500

    https://github.com/elmsln/issues/issues/1929

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -29,29 +29,40 @@ for project in */ ; do
   # if it was a folder, then this will just fail without an issue
   mkdir ../../node_modules/@lrnwebcomponents/${p}
   if [ "${machine}" == "MinGw" ]; then
+    # Check and create symlink function for Windows Git Bash
+    create_symlink_win() {
+      target="$1"
+      link="$2"
+      if [[ ! -e "$link" ]]; then
+        cmd //c mklink //d "$(cygpath -w "$link")" "$(cygpath -w "$target")"
+      elif [[ -L "$link" && "$(readlink "$link")" != "$target" ]]; then
+        rm "$link"
+        cmd //c mklink //d "$(cygpath -w "$link")" "$(cygpath -w "$target")"
+      fi
+    }
     if [ -f "${p}.js" ]; then
-      symlink-dir ../../../elements/${p}/${p}.js ../../node_modules/@lrnwebcomponents/${p}/${p}.js
+      create_symlink_win ../../../elements/${p}/${p}.js ../../node_modules/@lrnwebcomponents/${p}/${p}.js
     fi
     if [ -f "package.json" ]; then
-      symlink-dir ../../../elements/${p}/package.json ../../node_modules/@lrnwebcomponents/${p}/package.json
+      create_symlink_win ../../../elements/${p}/package.json ../../node_modules/@lrnwebcomponents/${p}/package.json
     fi
     if [ -d "lib" ]; then
-      symlink-dir ../../../elements/${p}/lib ../../node_modules/@lrnwebcomponents/${p}/lib
+      create_symlink_win ../../../elements/${p}/lib ../../node_modules/@lrnwebcomponents/${p}/lib
     fi
     if [ -d "locales" ]; then
-      symlink-dir ../../../elements/${p}/locales ../../node_modules/@lrnwebcomponents/${p}/locales
+      create_symlink_win ../../../elements/${p}/locales ../../node_modules/@lrnwebcomponents/${p}/locales
     fi
     if [ -d "server" ]; then
-      symlink-dir ../../../elements/${p}/server ../../node_modules/@lrnwebcomponents/${p}/server
+      create_symlink_win ../../../elements/${p}/server ../../node_modules/@lrnwebcomponents/${p}/server
     fi
     if [ -d "build" ]; then
-      symlink-dir ../../../elements/${p}/build ../../node_modules/@lrnwebcomponents/${p}/build
+      create_symlink_win ../../../elements/${p}/build ../../node_modules/@lrnwebcomponents/${p}/build
     fi
     if [ -d "src" ]; then
-      symlink-dir ../../../elements/${p}/src ../../node_modules/@lrnwebcomponents/${p}/src
+      create_symlink_win ../../../elements/${p}/src ../../node_modules/@lrnwebcomponents/${p}/src
     fi
     if [ -d "dist" ]; then
-      symlink-dir ../../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
+      create_symlink_win ../../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
     fi
   else
     if [ -f "${p}.js" ]; then
@@ -78,6 +89,18 @@ for project in */ ; do
     if [ -d "dist" ]; then
        ln -s ../../../elements/${p}/dist ../../node_modules/@lrnwebcomponents/${p}/dist
     fi
+  fi
+  if [ -d "src/src" ]; then
+    echo "Found nested src/src in $project, deleting..."
+    unlink src/src
+  fi
+  if [ -d "lib/lib" ]; then
+    echo "Found nested lib/lib in $project, deleting..."
+    unlink lib/lib
+  fi
+  if [ -d "locales/locales" ]; then
+    echo "Found nested locales/locales in $project, deleting..."
+    unlink locales/locales
   fi
   cd ../
 done
