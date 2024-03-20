2f71df9a10d15d532b4091d41d5c03b3fc1d2acc
Keith Charlton <kcc5354@psu.edu>
2024-03-20 16:55:43 -0400

    hr fix

--- elements/d-d-d/lib/DDDStyles.js
+++ elements/d-d-d/lib/DDDStyles.js
@@ -621,15 +621,18 @@ export const DDDReset = css`
   }
   .hr-vert {
     border-bottom: none;
-    border-left: 3px solid var(--ddd-theme-primary);
+    border-left: var(--ddd-theme-header-border-thickness) solid var(--ddd-theme-primary);
     padding-left: var(--ddd-spacing-3);
   }
-  .hr-horz {
+  .hr-horz::after {
+    content: "";
     width: var(--ddd-theme-header-border-treatment);
+    border-bottom: var(--ddd-theme-header-border-thickness) solid var(--ddd-theme-primary);
+    height: 0;
+    display: block;
+    padding-top: var(--ddd-spacing-2);
   }
-  h2 > hr {
-    margin-top: var(--ddd-spacing-4);
-  }
+
   .ddd-theme-header-border-thickness-0{--ddd-theme-header-border-thickness:var(--ddd-theme-header-border-thickness-0);}
   .ddd-theme-header-border-thickness-xs{--ddd-theme-header-border-thickness:var(--ddd-theme-header-border-thickness-xs);}
   .ddd-theme-header-border-thickness-sm{--ddd-theme-header-border-thickness:var(--ddd-theme-header-border-thickness-sm);}
