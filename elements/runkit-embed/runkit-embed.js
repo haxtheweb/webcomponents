170a4c1d8986bf6e437a51c903447e20f98bc8c1
btopro <btopro@outlook.com>
2024-04-26 15:33:43 -0400

    https://github.com/elmsln/issues/issues/1983

--- elements/runkit-embed/runkit-embed.js
+++ elements/runkit-embed/runkit-embed.js
@@ -2,8 +2,9 @@
  * Copyright 2024
  * @license Apache-2.0, see License.md for full text.
  */
-import { LitElement, html } from "lit";
+import { html, css } from "lit";
 import { ESGlobalBridgeStore } from "@lrnwebcomponents/es-global-bridge/es-global-bridge.js";
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 
 /**
  * `runkit-embed`
@@ -11,12 +12,23 @@ import { ESGlobalBridgeStore } from "@lrnwebcomponents/es-global-bridge/es-globa
  * @demo demo/index.html
  * @element runkit-embed
  */
-class RunkitEmbed extends LitElement {
+class RunkitEmbed extends DDD {
   /**
    * Convention we use
    */
   static get tag() {
     return "runkit-embed";
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+          min-height: 100px;
+        }
+      `,
+    ];
   }
 
   /**
@@ -25,25 +37,49 @@ class RunkitEmbed extends LitElement {
   constructor() {
     super();
     this.__runkitloaded = false;
-    ESGlobalBridgeStore.load(
-      "runkit",
-      "https://embed.runkit.com"
-    ).then(() => {
+    this.loading = false;
+    ESGlobalBridgeStore.load("runkit", "https://embed.runkit.com").then(() => {
       if (globalThis.RunKit) {
         this.__runkitloaded = true;
         this.buildRunKit();
       }
     });
+    if (this.querySelector("template")) {
+      this.source = this.querySelector("template").content.textContent;
+    } else {
+      this.source = "";
+    }
     this.mode = "endpoint";
     this.nodeVersion = "18.x.x";
-    this.source = this.querySelector('template').content.textContent;
+    this.__observer = new MutationObserver((mutations) => {
+      if (this.querySelector("template") && !this.loading) {
+        this.source = this.querySelector("template").content.textContent;
+      }
+    });
+    this.__observer.observe(this, {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: false,
+    });
   }
 
   updated(changedProperties) {
     if (super.updated) {
       super.updated(changedProperties);
     }
-    if ((changedProperties.has('nodeVersion') || changedProperties.has('mode') && this.nodeVersion && this.mode)) {
+    // ensure we DO NOT update when in hax mode or we're in trouble
+    if (
+      !this._haxstate &&
+      !this.dataHaxActive &&
+      this.__runkitloaded &&
+      (changedProperties.has("source") ||
+        changedProperties.has("nodeVersion") ||
+        changedProperties.has("mode")) &&
+      this.nodeVersion &&
+      this.mode &&
+      this.source
+    ) {
       this.buildRunKit();
     }
   }
@@ -60,9 +96,11 @@ class RunkitEmbed extends LitElement {
         element: this,
         source: this.source,
         onLoad: (item) => {
-          this.__rkContainer = this.querySelector(`[name="${item.name}"]`).parentNode;
+          this.__rkContainer = this.querySelector(
+            `[name="${item.name}"]`,
+          ).parentNode;
           this.loading = false;
-        }
+        },
       });
     }
   }
@@ -73,11 +111,46 @@ class RunkitEmbed extends LitElement {
 
   static get properties() {
     return {
-      nodeVersion: { type: String, attribute: 'node-version'},
-      mode: { type: String, },
+      nodeVersion: { type: String, attribute: "node-version" },
+      mode: { type: String },
       loading: { type: Boolean },
       source: { type: String },
+      dataHaxActive: {
+        type: String,
+        reflect: true,
+        attribute: "data-hax-active",
+      },
+    };
+  }
+
+  /**
+   * Implements haxHooks to tie into life-cycle if hax exists.
+   */
+  haxHooks() {
+    return {
+      preProcessNodeToContent: "haxpreProcessNodeToContent",
+      editModeChanged: "haxeditModeChanged",
+    };
+  }
+
+  haxeditModeChanged(value) {
+    this._haxstate = value;
+    if (this._haxstate && this.querySelector("div")) {
+      this.querySelector("div").remove();
     }
+  }
+
+  // about to convert to content, ensure we are no longer the editable-table
+  async haxpreProcessNodeToContent(node) {
+    this.__runkitloaded = false;
+    this.loading = false;
+    if (this.__rkContainer) {
+      this.__rkContainer.remove();
+    }
+    if (this._haxstate && this.querySelector("div")) {
+      this.querySelector("div").remove();
+    }
+    return node;
   }
 
   /**
