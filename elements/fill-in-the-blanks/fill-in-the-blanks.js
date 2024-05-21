91e960badcc27ac25ae79fc79f5295a700006483
btopro <btopro@outlook.com>
2024-05-21 16:44:48 -0400

    https://github.com/elmsln/issues/issues/2001

--- elements/fill-in-the-blanks/fill-in-the-blanks.js
+++ elements/fill-in-the-blanks/fill-in-the-blanks.js
@@ -2,8 +2,8 @@
  * Copyright 2024
  * @license Apache-2.0, see License.md for full text.
  */
-import { LitElement, html, css } from "lit";
-import { MatchingQuestion } from "@lrnwebcomponents/matching-question/matching-question.js";
+import { html, css } from "lit";
+import { MarkTheWords } from "@lrnwebcomponents/mark-the-words/mark-the-words.js";
 
 /**
  * `fill-in-the-blanks`
@@ -11,7 +11,7 @@ import { MatchingQuestion } from "@lrnwebcomponents/matching-question/matching-q
  * @demo demo/index.html
  * @element fill-in-the-blanks
  */
-class FillInTheBlanks extends MatchingQuestion {
+class FillInTheBlanks extends MarkTheWords {
   /**
    * Convention we use
    */
@@ -19,11 +19,56 @@ class FillInTheBlanks extends MatchingQuestion {
     return "fill-in-the-blanks";
   }
 
+  static get styles() {
+    return [super.styles, css`
+    
+    simple-fields-field {
+      display: inline-block;
+      width: 120px;
+      margin-bottom: 0;
+      vertical-align: middle;
+      padding: var(--ddd-spacing-1) var(--ddd-spacing-2);
+    }`];
+  }
+
   /**
    * HTMLElement
    */
   constructor() {
     super();
+  }
+
+  renderInteraction() {
+    return html`<div class="text-wrap"><div class="text">
+      ${this.wordList.map(word => html`
+      ${word.text.startsWith('[') && word.text.endsWith(']') ? this.renderFillInBlankField(word.text.replace('[','').replace(']','')) : html`${word.text} `}
+      `)}
+      </div></div>`;
+  }
+
+  renderFillInBlankField(word) {
+    let ary = word.split("|");
+    if (ary.length !== 1) {
+      let selectItems = [{
+        text: "",
+        value: null,
+      }, ...ary.map((item) => {
+        return {
+          text: item,
+          value: item
+        };
+      })];
+    return html`
+      <simple-fields-field
+        type="select"
+        .itemsList="${selectItems}"
+      ></simple-fields-field>`;        
+    }
+    else {
+      return html`
+      <simple-fields-field type="text"></simple-fields-field>`;
+  
+    }
   }
 
   /**
