212ddc74ca63697a55bda34397eba319a0f7d19a
Zach Dodson <zdodson21@gmail.com>
2024-06-04 08:39:32 -0400

    engine switch progress (trying to fix git issue here)

---
+++ elements/chat-agent/lib/chat-developer-panel.js
@@ -0,0 +1,136 @@
+/**
+ * Copyright 2024 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+
+import { html, css } from "lit";
+import { DDD } from "@haxtheweb/d-d-d/d-d-d.js";
+
+class ChatDeveloperPanel extends DDD {
+
+  static get tag() {
+    return "chat-developer-panel";
+  }
+
+  constructor() {
+    super();
+    this.engine = "alfred";
+  }
+
+  static get styles() {
+    return[
+      super.styles,
+      css`
+        /* https://oer.hax.psu.edu/bto108/sites/haxcellence/documentation/ddd */
+
+        :host {
+          display: block;
+        }
+
+        .chat-developer-panel-wrapper {
+          background-color: var(--ddd-theme-default-keystoneYellow);
+          padding: var(--ddd-spacing-1) var(--ddd-spacing-1);
+          border-radius: var(--ddd-radius-sm);
+        }
+
+        .console-table {
+          display: flex;
+          gap: var(--ddd-spacing-1);
+          justify-content: space-between;
+          align-items: center;
+        }
+
+        button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+          gap: var(--ddd-spacing-1);
+        }
+
+      `
+    ];
+  }
+
+  render() {
+    let oppositeEngine = this.engine === "alfred" ? "robin" : "alfred";
+    
+    return html`
+      <div class="chat-developer-panel-wrapper">
+        <div class="console-table">
+          <button id="console-table-user">
+            <div class="button-icon">
+              <simple-icon-lite icon="hax:console-line"></simple-icon-lite>
+            </div>
+            <div class="button-text">
+              console.table() user chat log
+            </div> 
+          </button>
+
+          <button id="console-table-merlin">
+            <div class="button-icon">
+              <simple-icon-lite icon="hax:console-line"></simple-icon-lite> 
+            </div>
+            <div class="button-text">
+              console.table() merlin chat log
+            </div>
+          </button>
+
+          <button id="console-table-all">
+            <div class="button-icon">
+              <simple-icon-lite icon="hax:console-line"></simple-icon-lite>
+            </div>
+            <div class="button-text">
+              console.table() entire chat log
+            </div>
+          </button>
+        </div>
+
+        <div class="switch-engine-controls">
+          <button id="switch-engine-btn">
+          <div class="button-icon">
+              <simple-icon-lite icon="hardware:memory"></simple-icon-lite>
+            </div>
+            <div class="button-text" @click=${this.handleSwitchEngineButton}>
+              Switch LLM Engine (Current Engine = <em>${this.engine}</em>)
+            </div>
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  handleSwitchEngineButton(e) {
+    console.info("switch engine button clicked");
+    
+    switch (this.engine) {
+      case "alfred":
+        this.engine = "robin";
+        break;
+      case "robin":
+        this.engine = "alfred";
+        break;
+    }
+
+    console.info("engine switched to " + this.engine);
+
+    e.target.innerHTML = `Switch LLM Engine (Current Engine = <em>${this.engine}</em>)`;
+  }
+
+  static get properties() {
+    return {
+      ...super.properties,
+    };
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(ChatDeveloperPanel.tag, ChatDeveloperPanel);
+export { ChatDeveloperPanel };
