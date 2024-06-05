e4dd32c4f0e420e4bbeae71e954aa76f62a29fbe
Zach Dodson <zdodson21@gmail.com>
2024-06-05 06:11:25 -0400

    added handlers for buttons + dev mode support

--- elements/chat-agent/lib/chat-control-bar.js
+++ elements/chat-agent/lib/chat-control-bar.js
@@ -15,6 +15,7 @@ class ChatControlBar extends DDD {
     super();
 
     this._isFullView = false;
+    this.developerModeEnabled = false;
   }
 
   static get styles() {
@@ -44,10 +45,10 @@ class ChatControlBar extends DDD {
       
       <div class="chat-control-bar-wrapper">
         <div class="left-side">
-          <button id="download-button">
+          <button id="download-button" @click=${this.handleDownloadLogButton}>
             <simple-icon-lite icon="icons:file-download"></simple-icon-lite> 
           </button>
-          <button id="reset-button">
+          <button id="reset-button" @click=${this.handleResetButton}>
             <simple-icon-lite icon="icons:refresh"></simple-icon-lite>
           </button>
         </div>
@@ -55,7 +56,7 @@ class ChatControlBar extends DDD {
           <button id="view-button" @click=${this.handleViewButton}>
             <simple-icon-lite icon="${this._isFullView ? 'icons:fullscreen-exit' : 'icons:fullscreen'}"></simple-icon-lite>
           </button>
-          <button id="hide-button">
+          <button id="hide-button" @click=${this.handleHideButton}>
             <simple-icon-lite icon="lrn:arrow-right"></simple-icon-lite> <!-- TODO might want to change to just be minimize button, since there shouldn't be a way to shut down AI without refreshing page -->
           </button>
         </div>
@@ -64,10 +65,77 @@ class ChatControlBar extends DDD {
   }
 
   /**
+   * @description handles the functionality of the download button
+   */
+  handleDownloadLogButton() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: Download log button pressed.');
+    }
+
+    this.downloadChatLog();
+  }
+
+  /**
+   * @description handles the functionality of the reset button
+   */
+  handleResetButton() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: Reset button pressed.');
+    }
+
+    //TODO prompts Merlin to pop up an alert asking if the user would like to download the chat log
+
+    if (confirm('Would you like to download the chat log before you reset?')) {
+      if(this.developerModeEnabled) {
+        console.info('HAX-DEV-MODE: Download before reset confirmed.');
+      }
+      this.downloadChatLog();
+    } else {
+      if(this.developerModeEnabled) {
+        console.info('HAX-DEV-MODE: Download before reset denied.');
+      }
+    }
+
+    this.resetChat();
+  }
+
+  /**
    * @description Toggles the view of chat-interface to full or minimized
    */
   handleViewButton() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: View switch button pressed.');
+    }
+
     this._isFullView = !this._isFullView;
+
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: View switched to: ' + (this._isFullView ? 'full' : 'minimized'));
+    }
+  }
+
+  handleHideButton() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: Hide button pressed.');
+    }
+  }
+
+  /**
+   * @description downloads the chat log
+   */
+  downloadChatLog() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: downloadChatLog() called.');
+    }
+  }
+
+  /**
+   * @description resets the chat
+   */
+  resetChat() {
+    if (this.developerModeEnabled) {
+      console.info('HAX-DEV-MODE: resetChat() called.');
+    }
   }
 
   static get properties() {
@@ -77,6 +145,11 @@ class ChatControlBar extends DDD {
       _isFullView: {
         type: Boolean,
         attribute: "full-view",
+      },
+
+      developerModeEnabled: {
+        type: Boolean,
+        attribute: "developer-mode",
       },
     };
   }
