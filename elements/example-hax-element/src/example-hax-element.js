149c9b32403a9bde4fec3aebd95c49935b54c195
btopro <btopro@outlook.com>
2024-09-19 14:59:36 -0400

    https://github.com/haxtheweb/issues/issues/2149

--- elements/example-hax-element/src/example-hax-element.js
+++ elements/example-hax-element/src/example-hax-element.js
@@ -1,191 +1,58 @@
 /**
- * Copyright 2018 The Pennsylvania State University
+ * Copyright 2024 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
+import { LitElement, html, css } from "lit";
 /**
  * `example-hax-element`
  * @element example-hax-element
  * `Provide an example to pick apart of a working HAX element`
- *
- * @microcopy - language worth noting:
- *  -
- *
-
  * @demo demo/index.html
  */
-class ExampleHaxElement extends HTMLElement {
-  // render function
-  render() {
-    return html` <style>
-        :host {
-          display: block;
-        }
-
-        :host([hidden]) {
-          display: none;
-        }
-      </style>
-      <slot></slot>`;
-  }
-
-  // haxProperty definition
-  static get haxProperties() {
-    return {
-      canScale: true,
-
-      canEditSource: true,
-      gizmo: {
-        title: "Example hax-element",
-        description:
-          "Provide an example to pick apart of a working HAX element",
-        icon: "icons:android",
-        color: "green",
-        tags: ["Other", "dev", "example"],
-        handles: [
-          {
-            type: "todo:read-the-docs-for-usage",
-          },
-        ],
-        meta: {
-          author: "You",
-          owner: "Your Company",
-        },
-      },
-      settings: {
-        configure: [
-          {
-            property: "title",
-            description: "",
-            inputMethod: "textfield",
-            required: false,
-            icon: "icons:android",
-          },
-          {
-            property: "available",
-            description: "",
-            inputMethod: "boolean",
-            required: false,
-            icon: "icons:android",
-          },
-        ],
-        advanced: [],
-      },
-    };
-  }
-  // properties available to the custom element for data binding
-  static get properties() {
-    return {
-      ...super.properties,
-
-      title: {
-        name: "title",
-        type: String,
-        value: "My Example",
-        reflectToAttribute: false,
-        observer: false,
-      },
-      available: {
-        name: "available",
-        type: Boolean,
-        value: "",
-        reflectToAttribute: false,
-        observer: false,
-      },
-    };
-  }
-
-  /**
-   * Store the tag name to make it easier to obtain directly.
-   * @notice function name must be here for tooling to operate correctly
-   */
+export class ExampleHaxElement extends LitElement {
+  // convention our team enjoys
   static get tag() {
     return "example-hax-element";
   }
-  /**
-   * life cycle
-   */
-  constructor(delayRender = false) {
+
+  constructor() {
     super();
+    this.title = null;
+    this.shiny = false;
+  }
 
-    // set tag for later use
-    this.tag = ExampleHaxElement.tag;
-    // map our imported properties json to real props on the element
-    // @notice static getter of properties is built via tooling
-    // to edit modify src/ExampleHaxElement-properties.json
-    let obj = ExampleHaxElement.properties;
-    for (let p in obj) {
-      if (obj.hasOwnProperty(p)) {
-        if (this.hasAttribute(p)) {
-          this[p] = this.getAttribute(p);
-        } else {
-          this.setAttribute(p, obj[p].value);
-          this[p] = obj[p].value;
-        }
+  static get properties() {
+    return {
+      title: {
+        type: String
+      },
+      shiny: {
+        type: Boolean,
+        reflect: true
       }
-    }
-    // optional queue for future use
-    this._queue = [];
-    this.template = globalThis.document.createElement("template");
-
-    this.attachShadow({ mode: "open" });
-
-    if (!delayRender) {
-      this.render();
-    }
-  }
-  /**
-   * life cycle, element is afixed to the DOM
-   */
-  connectedCallback() {
-    if (globalThis.ShadyCSS) {
-      globalThis.ShadyCSS.styleElement(this);
-    }
-
-    if (this._queue.length) {
-      this._processQueue();
-    }
+    };
   }
 
-  _copyAttribute(name, to) {
-    const recipients = this.shadowRoot.querySelectorAll(to);
-    const value = this.getAttribute(name);
-    const fname = value == null ? "removeAttribute" : "setAttribute";
-    for (const node of recipients) {
-      node[fname](name, value);
-    }
-  }
-
-  _queueAction(action) {
-    this._queue.push(action);
-  }
-
-  _processQueue() {
-    this._queue.forEach((action) => {
-      this[`_${action.type}`](action.data);
-    });
-
-    this._queue = [];
-  }
-
-  _setProperty({ name, value }) {
-    this[name] = value;
+  static get styles() {
+    return [css`
+      :host {
+        display: block;
+      }
+      :host([shiny]) h2 {
+        background-color: var(--ddd-theme-accent);
+      }
+    `];
   }
 
   render() {
-    this.shadowRoot.innerHTML = null;
-    this.template.innerHTML = this.html;
-
-    if (globalThis.ShadyCSS) {
-      globalThis.ShadyCSS.prepareTemplate(this.template, this.tag);
-    }
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+    return html`<h2>${this.title}</h2>`;
   }
 
-  //static get observedAttributes() {
-  //  return [];
-  //}
-  // disconnectedCallback() {}
-  // attributeChangedCallback(attr, oldValue, newValue) {}
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
+  }
 }
-customElements.define(ExampleHaxElement.tag, ExampleHaxElement);
-export { ExampleHaxElement };
+globalThis.customElements.define(ExampleHaxElement.tag, ExampleHaxElement);
