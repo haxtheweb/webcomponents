69d9159448cdcf0cf3fcc4e90354ba8aab119951
btopro <bto108@psu.edu>
2025-02-03 10:40:28 -0500

    https://github.com/haxtheweb/issues/issues/392

--- elements/clean-portfolio-theme/clean-portfolio-theme.js
+++ elements/clean-portfolio-theme/clean-portfolio-theme.js
@@ -4,7 +4,6 @@
  */
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
-import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `clean-portfolio-theme`
@@ -12,7 +11,7 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
  * @demo index.html
  * @element clean-portfolio-theme
  */
-export class CleanPortfolioTheme extends DDDSuper(I18NMixin(LitElement)) {
+export class CleanPortfolioTheme extends DDDSuper(LitElement) {
 
   static get tag() {
     return "clean-portfolio-theme";
@@ -21,18 +20,6 @@ export class CleanPortfolioTheme extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/clean-portfolio-theme.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
   }
 
   // Lit reactive properties
@@ -53,32 +40,16 @@ export class CleanPortfolioTheme extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--clean-portfolio-theme-label-font-size, var(--ddd-font-size-s));
-      }
     `];
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+  <h1>UPDATE ME TO BE THE REAL THING</h1>
+    `;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
-  }
 }
 
 globalThis.customElements.define(CleanPortfolioTheme.tag, CleanPortfolioTheme);
