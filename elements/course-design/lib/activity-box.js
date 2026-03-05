8a534bbd2dada2cea24fe092a2483ebf293f6e9a
Keith Charlton <charltonkeith8@gmail.com>
2026-03-04 22:24:06 -0500

    slighht import fix

--- elements/course-design/lib/activity-box.js
+++ elements/course-design/lib/activity-box.js
@@ -1,5 +1,5 @@
 import { DDD } from "@haxtheweb/d-d-d/d-d-d.js";
-import { DDDDataAttributes } from "@haxtheweb/d-d-d/lib/DDDStyles";
+import { DDDDataAttributes } from "@haxtheweb/d-d-d/lib/DDDStyles.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 import { css, html } from "lit";
 
