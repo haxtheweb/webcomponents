ae6a03bee379e0cd0b3b21742f4e621331312e24
winstonwumbo <me@winstonwumbo.com>
2026-03-22 15:30:36 -0400

    Refactored haxthewebme.sh to install bzip2/NVM, minimal yarn dependencies

--- scripts/haxthewebme.sh
+++ scripts/haxthewebme.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+if ! command -v bzip2 &>/dev/null;then
+  echo "bzip2 dependency is missing"
+
+  # Git Bash/Cygwin provide bzip2 by default
+  if command -v apt-get &>/dev/null; then
+    echo "Installing with apt..."
+    sudo apt-get update -qq && sudo apt-get install -y bzip2
+  elif command -v dnf &>/dev/null; then
+    echo "Installing with dnf..."
+    sudo dnf install -y bzip2
+  elif command -v yum &>/dev/null; then
+    echo "Installing with yum..."
+    sudo yum install -y bzip2
+  elif command -v zypper &>/dev/null; then
+    echo "Installing with zypper..."
+    sudo zypper install -y bzip2
+  elif command -v pacman &>/dev/null; then
+    echo "Installing with pacman..."
+    sudo pacman -Sy --noconfirm bzip2
+  else
+    echo "Error: No supported package manager found. Please install bzip2"
+    exit
+  fi
+fi
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
@@ -10,29 +36,47 @@ esac
 
 # make sure node is installed
 if ! command -v node;then
-  echo "Install node and npm first then re-run script"
-  echo "Go to https://nodejs.org/en/download/ to download and install"
-  exit
-fi
+  if [ "${machine}" == "Cygwin" ]; then
+    echo "Install node and npm first then re-run script"
+    echo "Go to https://nodejs.org/en/download/ to download and install"
+    exit
+  elif [ "${machine}" == "MinGw" ]; then
+    echo "Install node and npm first then re-run script"
+    echo "Go to https://nodejs.org/en/download/ to download and install"
+    exit
+  else
+    while true; do
+      read -rp "Download and run nvm install script? [y/n]: " confirm
+      confirm_lowercase="${confirm,,}"
 
-clone_and_install () {
-  git clone https://github.com/haxtheweb/webcomponents.git
-  cd webcomponents
-  yarn global add symlink-dir
-  yarn global add @haxtheweb/create
-  yarn global add @web/test-runner
-  yarn global add @web/test-runner-commands
-  yarn global add @web/test-runner-puppeteer
-  yarn global add @web/test-runner-playwright
-  yarn global add lerna
-  yarn global add web-component-analyzer
-  yarn install
-}
+      if [[ "$confirm_lowercase" == "y" || "$confirm_lowercase" == "yes" ]]; then
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+        \. "$HOME/.nvm/nvm.sh"
+        nvm install 24
+        break
+      elif [[ "$confirm_lowercase" == "n" || "$confirm_lowercase" == "no" ]]; then
+        echo "Install node and npm first then re-run script"
+        echo "Go to https://nodejs.org/en/download/ to download and install"
+        exit
+      else
+        echo "Please enter yes or no."
+      fi
+  done
+  fi
+fi
 
 # if yarn isn't installed install it
 if ! command -v yarn;then
   npm -g install yarn
 fi
+
+clone_and_install () {
+  git clone https://github.com/haxtheweb/webcomponents.git
+  cd webcomponents
+  npm install -g add @haxtheweb/create
+  yarn global add web-component-analyzer
+  yarn install
+}
 
 if [ "${machine}" == "Cygwin" ]; then
   git config --global core.autocrlf true
